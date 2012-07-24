60681b1a2899c653dfdda5d5274a5fb646afaa93
Joshua Peek <josh@joshpeek.com>
2012-07-24 13:01:56 -0500

    Ensure bare url() helper isn't used in primer itself

--- test/test_primer.rb
+++ test/test_primer.rb
@@ -4,7 +4,18 @@ require 'primer/style'
 class TestPrimer < Test::Unit::TestCase
   include Primer::Style
 
+  def setup
+    @nodes = parse_files(Primer::APP)
+  end
+
   def test_style
-    assert_style parse_files(Primer::APP)
+    assert_style @nodes
+  end
+
+  def test_asset_url_helper
+    iterate_nodes(@nodes) do |node|
+      next unless node.is_a?(Sass::Tree::PropNode)
+      assert_no_match(/\surl\(/, node.to_scss, "#{node.filename}:#{node.line}")
+    end
   end
 end
