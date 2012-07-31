5d358bb9b1c2adc42828876f1571d81a5b551dc7
Joshua Peek <josh@joshpeek.com>
2012-07-31 13:36:44 -0500

    Ensure filename is set on sass nodes

--- lib/primer/style.rb
+++ lib/primer/style.rb
@@ -15,6 +15,8 @@ module Primer
       Dir["#{root}/**/*.scss"].each do |path|
         data = File.read(path)
         nodes << Sass::SCSS::Parser.new(data, path).parse
+        # sass wtf
+        nodes.each { |node| node.options = {:filename => path} }
       end
       nodes
     end
@@ -52,9 +54,8 @@ module Primer
     # Check if any CSS rules use js- classes or ids.
     def assert_no_js_rules(node)
       return unless node.is_a?(Sass::Tree::RuleNode)
-      fileref = "#{node.filename}:#{node.line} - " if node.filename
       assert_no_match(/(\#|\.)js-/,
-        node.rule.first, "#{fileref}CSS selectors can't start with js-. See http://is.gd/eFcrSg")
+        node.rule.first, "#{node.filename}:#{node.line} - CSS selectors can't start with js-. See http://is.gd/eFcrSg")
     end
   end
 end
