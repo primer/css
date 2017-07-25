f321b410c7ea2896c0332200b43d265d48832afe
Jon Rohan <rohan@github.com>
2017-07-25 14:52:18 -0700

    Updating test, and adding to all modules

--- tests/modules/test-document-styles.js
+++ tests/modules/test-document-styles.js
@@ -1,12 +1,15 @@
 const test = require("ava")
 const css = require(process.env.PWD)
+const fs = require("fs")
+const glob = require("glob")
 
 var selectors, classnames = null
+const classRegex = /class="([^"]+)"/ig
 
 function uniqueSelectors(s) {
   return s.filter(s => {
     // remove any selectors with hover states
-    return !s.includes(":")
+    return !s.includes(":") && !s.includes(".")
   }).map(s => {
     // split multi-selectors into last class used .foo .bar .baz
     return s.split(" ").pop()
@@ -14,7 +17,16 @@ function uniqueSelectors(s) {
 }
 
 function documentedClassnames(sources) {
-  return sources
+  var cn = []
+  sources.forEach( f => {
+    glob.sync(f).forEach( g => {
+      var match = null
+      while ((match = classRegex.exec(fs.readFileSync(g).toString())) != null) {
+        cn = cn.concat(match[1].split(" "))
+      }
+    })
+  })
+  return [...new Set(cn)]
 }
 
 test.before(async t => {
@@ -25,8 +37,12 @@ test.before(async t => {
   ])
 })
 
-test("builds test css", t => {
-  console.log(selectors);
-  console.log(classnames);
-  t.truthy(false)
+test("Every selector class is documented", t => {
+  var undocumented = []
+  selectors.forEach( selector => {
+    if (!classnames.includes(selector.replace(".", ""))) {
+      undocumented.push(selector)
+    }
+  })
+  t.is(undocumented.length, 0, `I did not find documentation for the "${undocumented.join(", ")}" selector(s) in the ${process.env.npm_package_name} module.`);
 })
