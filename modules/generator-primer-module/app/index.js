8b9f8ded6c49cdf1389770a6276a28b5244313a6
Shawn Allen <shawn.allen@github.com>
2017-08-31 22:23:17 -0700

    stub out yeoman generator

---
+++ modules/generator-primer-module/app/index.js
@@ -0,0 +1,181 @@
+const chalk = require("chalk")
+const fs = require("fs")
+const path = require("path")
+const Generator = require("yeoman-generator")
+const capitalize = require("./lib/capitalize")
+
+const META_PACKAGES = 
+
+module.exports = class PrimerModule extends Generator {
+
+  initializing() {
+    this.log("It looks like you are adding a new Primer module!")
+
+    this.argument("module", {
+      desc: "The name of your module (on npm)",
+      type: String,
+      required: false,
+    })
+
+    // assign defaults
+    Object.assign(this.options, {
+      type: "css",
+      status: "Experimental",
+    })
+  }
+
+  prompting() {
+    if (this.options.module) {
+      this.log("Great, let's get you started with %s...", this.options.module)
+    }
+
+    let prompts = [
+      {
+        name: "module",
+        message: "What should the module name be (on npm)?",
+        type: "input",
+      },
+      {
+        name: "title",
+        message: "What should the title be (for humans)?",
+        type: "input",
+        default: (answers) => {
+          return capitalize(
+            (answers.module || this.options.module).replace("primer-", "")
+          )
+        },
+      },
+      {
+        name: "description",
+        message: "Describe your module in a single sentence. (This will go into the package.json and README.md.)",
+        type: "input",
+        default: "TODO: fill in this description later",
+      },
+      {
+        name: "dependents",
+        message: "Which meta-package(s) should we add this to?",
+        type: "checkbox",
+        choices: [
+          "primer-css",
+          "primer-core",
+          "primer-product",
+          "primer-marketing",
+        ],
+        default: ["primer-css"],
+      },
+      {
+        type: "input",
+        message: "Where can we find the docs? (We'll read this file from the path you provide.)",
+        name: "docs",
+        validate: (filePath) => {
+          if (!filePath) {
+            return true
+          }
+          return fs.existsSync(filePath) ||
+            `No such file: "${filePath}" in ${process.cwd()}`
+        },
+      },
+    ]
+
+    // remove prompts for which arguments were already provided
+    prompts = prompts.filter(prompt => {
+      return !(prompt.name in this.options)
+    })
+
+    return this.prompt(prompts)
+      .then(answers => {
+        Object.assign(this.options, answers)
+      })
+  }
+
+  configuring() {
+    this.options.docs = this.options.docs
+      ? fs.readFileSync(this.options.docs, "utf8")
+      : "TODO: add docs here"
+    this.options.dependencies = this._getDependencies()
+  }
+
+  paths() {
+    this.basePath = this.destinationPath(this.options.module)
+  }
+
+  writing() {
+    this.log("creating: %s", chalk.green(this.basePath))
+
+    const data = [
+      "module",
+      "dependencies",
+      "description",
+      "docs",
+      "status",
+      "title",
+      "type",
+    ].reduce((acc, key) => {
+      acc[key] = this.options[key]
+      return acc
+    }, {})
+
+    // this.log("data:", JSON.stringify(data, null, "  "))
+
+    // copy the whole directory with each file treated as
+    // an EJS template
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.basePath,
+      data
+    )
+  }
+
+  install() {
+    const pkg = this.fs.readJSON(
+      path.join(this.basePath, "package.json")
+    )
+    // this.log("package:", pkg.name, "@", pkg.version)
+
+    this.options.dependents.forEach(dependent => {
+      this._addAsDependencyTo(pkg, dependent)
+    })
+
+    this.spawnCommandSync("npm", ["run", "bootstrap"], {
+      stdio: "inherit",
+    })
+  }
+
+  end() {
+    this.log(
+      "\n%s\n\n%s",
+      chalk.green("Ready to roll!"),
+      chalk.yellow("Remember to fill in real stuff if you have any TODOs listed below:")
+    )
+    this.spawnCommandSync("ack", ["TODO", this.basePath], {
+      stdio: "inherit",
+    })
+  }
+
+  _getDependencies() {
+    return [
+      "primer-support",
+    ].reduce((deps, module) => {
+      deps[module] = require(`${module}/package.json`)
+      return deps
+    }, {})
+  }
+
+  _addAsDependencyTo(pkg, dest) {
+    this.log(
+      "adding %s@%s as a dependency to %s...",
+      pkg.name, pkg.version, dest,
+      pkg
+    )
+
+    const destPath = require.resolve(
+      path.join(dest, "package.json")
+    )
+
+    this.fs.extendJSON(destPath, {
+      dependencies: {
+        [pkg.name]: pkg.version,
+      },
+    })
+  }
+}
