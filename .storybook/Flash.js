53181558eed26e3ab6a1765bbcc9310f17975e93
Diana Mounter <broccolini@github.com>
2017-09-02 18:01:47 -0400

    add flash with action button and flash-full

--- .storybook/Flash.js
+++ .storybook/Flash.js
@@ -30,3 +30,20 @@ storiesOf('Flash alerts', module)
     </div>
   </div>
 ))
+.add('flash with action button', () => (
+  <div className='p-4'>
+    <div className="flash">
+      <button type="submit" className="btn btn-sm primary flash-action">Complete action</button>
+      Flash message with action here.
+    </div>
+  </div>
+))
+.add('flash-full', () => (
+  <div className='p-4'>
+    <div className="container-lg">
+      <div className="flash flash-full">
+        Full width flash message.
+      </div>
+    </div>
+  </div>
+))
