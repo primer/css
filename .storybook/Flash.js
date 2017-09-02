4743c82abd4c99fb52a83e8e4b118406c8429652
Diana Mounter <broccolini@github.com>
2017-09-02 17:54:52 -0400

    base flash styles

--- .storybook/Flash.js
+++ .storybook/Flash.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-storiesOf('Flash', module)
-.add('Flash', () => (
+storiesOf('Flash alerts', module)
+.add('flash', () => (
   <div className='p-4'>
     <div className='flash'>
-      Flash
+      flash
+    </div>
+  </div>
+))
+.add('flash-warn', () => (
+  <div className='p-4'>
+    <div className='flash flash-warn'>
+      flash-warn
+    </div>
+  </div>
+))
+.add('flash-error', () => (
+  <div className='p-4'>
+    <div className='flash flash-error'>
+      flash-error
+    </div>
+  </div>
+))
+.add('flash-success', () => (
+  <div className='p-4'>
+    <div className='flash flash-success'>
+      flash-success
     </div>
   </div>
 ))
