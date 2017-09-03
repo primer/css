09c340b3f1113e9e69e35331202555351c98297f
Diana Mounter <broccolini@github.com>
2017-09-03 12:19:45 -0400

    add CircleBadge and DashedConnection stores

--- .storybook/Avatars.js
+++ .storybook/Avatars.js
@@ -10,7 +10,7 @@ storiesOf('Avatars', module)
 ))
 .add('avatar-parent-child', () => (
   <div>
-    <div className="avatar-parent-child float-left">
+    <div className='avatar-parent-child float-left'>
       <img className='avatar'width='48' height='48' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
       <img className='avatar avatar-child' alt='Child cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
     </div>
@@ -25,4 +25,40 @@ storiesOf('Avatars', module)
     </div>
   </div>
 
+))
+.add('CircleBadge--small', () => (
+  <div>
+    <a className='CircleBadge CircleBadge--small' href='#url' title='Travis CI'>
+      <img src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png' className='CircleBadge-icon'/>
+    </a>
+  </div>
+))
+.add('CircleBadge--medium', () => (
+  <div>
+    <a className='CircleBadge CircleBadge--medium' href='#url' title='Travis CI'>
+      <img src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png' className='CircleBadge-icon'/>
+    </a>
+  </div>
+))
+.add('CircleBadge--large', () => (
+  <div>
+    <a className='CircleBadge CircleBadge--large' href='#url' title='Travis CI'>
+      <img src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png' className='CircleBadge-icon'/>
+    </a>
+  </div>
+))
+.add('DashedConnection', () => (
+  <div className='DashedConnection'>
+    <ul className='d-flex list-style-none flex-justify-between' aria-label='A sample GitHub workflow'>
+      <li className='CircleBadge CircleBadge--small' aria-label='GitHub'>
+        <img width='32' src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png'/>
+      </li>
+      <li className='CircleBadge CircleBadge--small' aria-label='Slack'>
+        <img width='32' src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png'/>
+      </li>
+      <li className='CircleBadge CircleBadge--small' aria-label='Travis CI'>
+        <img width='32' src='https://user-images.githubusercontent.com/334891/30004619-adc8ca86-90a0-11e7-8ccc-2322261d2ab1.png'/>
+      </li>
+    </ul>
+  </div>
 ))
