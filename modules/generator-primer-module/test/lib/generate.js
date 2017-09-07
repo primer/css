62017c11a0fe6404760a27ac320c7688c9626f78
Shawn Allen <shawn.allen@github.com>
2017-09-07 11:08:35 -0700

    add more useful docs for generate() test helper

--- modules/generator-primer-module/test/lib/generate.js
+++ modules/generator-primer-module/test/lib/generate.js
@@ -18,6 +18,28 @@ const TEST_PROMPTS = {
   "module": "primer-test"
 }
 
+/**
+ * Run the generator with an optional config that provides CLI options
+ * ("options"), positional arguments ("args"), and prompt answers ("prompts")
+ * in a temp directory. The return promise resolves with a function that acts
+ * like `path.join()` but prefixes the provided path parts with the path to the
+ * generated module directory, e.g.
+ *
+ * const generate = require("./lib/generate")
+ * generate().then(path => {
+ *   console.log("generated package.json:", path("package.json"))
+ * })
+ *
+ * // pass the module name as a positional argument
+ * generate({
+ *   args: ["primer-xyz"]
+ * })
+ *
+ * // or as a prompt answer
+ * generate({
+ *   prompts: {"module": "primer-xyz"}
+ * })
+ */
 module.exports = (config={}) => {
   const options = Object.assign({}, TEST_OPTS, config.options)
   const args = config.args || TEST_ARGS
@@ -28,7 +50,8 @@ module.exports = (config={}) => {
     .withPrompts(prompts)
     .inTmpDir()
     .then(tmpDir => {
-      // return a path function that joins to the temp dir
+      // return a path function that joins to the temp dir and the module name
+      // with the provided path parts
       return (...args) => path.join(tmpDir, prompts.module, ...args)
     })
 }
