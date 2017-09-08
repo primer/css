87038700335290a57c148cd2667e83ee17b7bbd9
Shawn Allen <shawn.allen@github.com>
2017-09-08 16:51:32 -0700

    add a note about duplicate postcss config

--- .storybook/postcss.config.js
+++ .storybook/postcss.config.js
@@ -1,3 +1,5 @@
+// FIXME this should go away when we merge and release:
+// <https://github.com/primer/primer-module-build/pull/19>
 const plugins = require("primer-module-build/lib/.postcss.json")
 delete plugins.use
 
