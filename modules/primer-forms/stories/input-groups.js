470662f103ef24759c7a48629965776a46a84b7f
Shawn Allen <shawn.allen@github.com>
2017-09-12 13:19:47 -0700

    nix form.p-4

--- modules/primer-forms/stories/input-groups.js
+++ modules/primer-forms/stories/input-groups.js
@@ -2,19 +2,17 @@ import React from 'react'
 import {Octicon} from '../../../.storybook/Octicon'
 
 export default () => (
-  <form className="p-4">
-    <div className="input-group">
-      <input
-        className="form-control"
-        type="text"
-        placeholder="Username"
-        aria-label="Username"
-      />
-      <span className="input-group-button">
-        <button className="btn" type="button" aria-label="Copy to clipboard">
-          <Octicon name="clippy" />
-        </button>
-      </span>
-    </div>
-  </form>
+  <div className="input-group">
+    <input
+      className="form-control"
+      type="text"
+      placeholder="Username"
+      aria-label="Username"
+    />
+    <span className="input-group-button">
+      <button className="btn" type="button" aria-label="Copy to clipboard">
+        <Octicon name="clippy" />
+      </button>
+    </span>
+  </div>
 )
