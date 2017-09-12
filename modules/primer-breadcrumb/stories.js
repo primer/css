ac2a619938079ccbdbe81a8f4fa6a4f49b2c123f
Shawn Allen <shawn.allen@github.com>
2017-09-12 16:43:19 -0700

    get stories from markdown!

--- modules/primer-breadcrumb/stories.js
+++ modules/primer-breadcrumb/stories.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import storiesFromMarkdown from '../../.storybook/lib/storiesFromMarkdown'
 
-storiesOf('Breadcrumb', module)
-.add('breadcrumb', () => (
-  <div className='p-4'>
-    <nav aria-label='Breadcrumb'>
-      <ol>
-        <li className='breadcrumb-item text-small'><a href='/business'>Business</a></li>
-        <li className='breadcrumb-item text-small'><a href='/business/customers'>Customers</a></li>
-        <li className='breadcrumb-item breadcrumb-item-selected text-small text-gray' aria-current='page'>MailChimp</li>
-      </ol>
-    </nav>
-  </div>
-))
+const stories = storiesOf('Breadcrumb', module)
+
+storiesFromMarkdown(require.context('.', true, /\.md$/))
+  .forEach((story, i) => {
+    stories.add(`story #${i + 1}`, () => story)
+  })
