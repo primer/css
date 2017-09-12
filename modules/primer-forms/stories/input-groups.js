9d703e08ac567e38ca2c22c48669f6496128bdc0
Shawn Allen <shawn.allen@github.com>
2017-09-12 13:10:15 -0700

    add copy to clipboard input group story

--- modules/primer-forms/stories/input-groups.js
+++ modules/primer-forms/stories/input-groups.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Octicon} from '../../../.storybook/Octicon'
 
 export default () => (
-  <div>
-    <h1>HI THERE</h1>
-  </div>
+  <form className="p-4">
+    <div className="input-group">
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Username"
+        aria-label="Username"
+      />
+      <span className="input-group-button">
+        <button className="btn" type="button" aria-label="Copy to clipboard">
+          <Octicon name="clippy" />
+        </button>
+      </span>
+    </div>
+  </form>
 )
