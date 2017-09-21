bebb56ba4af9783356d8a1568985bb4d1c44d442
Shawn Allen <shawn.allen@github.com>
2017-09-20 21:21:19 -0700

    Revert "Merge branch 'dev' into master"
    This reverts commit e15e1fdcaffabc1d6f051a915e9ee48cd101505f, reversing
    changes made to b0099723c700d1ef1ee984be04e8aa0188c958e5.

--- .storybook/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// FIXME this should go away when we merge and release:
-// <https://github.com/primer/primer-module-build/pull/19>
-const plugins = require("primer-module-build/lib/.postcss.json")
-delete plugins.use
-
-module.exports = {
-  plugins,
-}
