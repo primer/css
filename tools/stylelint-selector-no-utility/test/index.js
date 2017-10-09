fd2cb08fab84f674ad5a29cb147ddb6739b770fa
Jon Rohan <rohan@github.com>
2017-10-09 13:04:03 -0700

    Finishing no-utility import

--- tools/stylelint-selector-no-utility/test/index.js
+++ tools/stylelint-selector-no-utility/test/index.js
@@ -6,13 +6,25 @@ testRule(rule.rule, {
   config: true,
   skipBasicChecks: true,
   accept: [
-    { code: "a {  }" }
+    {
+      code: "a {  }"
+    }
   ],
   reject: [
-    { code: ".m-0 { color: #fff; }" },
-    { code: ".m-0:hover { color: #fff; }" },
-    { code: ".m-2, .foo { color: #fff; }" },
-    { code: ".m-2[type=button] { color: #fff; }" },
-    { code: ".foo.m-0 { color: #fff; }" }
+    {
+      code: ".m-0 { color: #fff; }"
+    },
+    {
+      code: ".m-0:hover { color: #fff; }"
+    },
+    {
+      code: ".m-2, .foo { color: #fff; }"
+    },
+    {
+      code: ".m-2[type=button] { color: #fff; }"
+    },
+    {
+      code: ".foo.m-0 { color: #fff; }"
+    }
   ]
 })
