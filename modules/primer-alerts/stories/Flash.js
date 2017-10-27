80515ee3a855ef9514ecba3d5069f0c2c981f3d4
Jon Rohan <rohan@github.com>
2017-10-27 09:33:52 -0700

    Revert these changes to focus on input groups only

--- modules/primer-alerts/stories/Flash.js
+++ modules/primer-alerts/stories/Flash.js
@@ -3,35 +3,47 @@ import { storiesOf } from '@storybook/react'
 
 storiesOf('Flash alerts', module)
 .add('flash', () => (
-  <div className='flash'>
-    flash
+  <div className='p-4'>
+    <div className='flash'>
+      flash
+    </div>
   </div>
 ))
 .add('flash-warn', () => (
-  <div className='flash flash-warn'>
-    flash-warn
+  <div className='p-4'>
+    <div className='flash flash-warn'>
+      flash-warn
+    </div>
   </div>
 ))
 .add('flash-error', () => (
-  <div className='flash flash-error'>
-    flash-error
+  <div className='p-4'>
+    <div className='flash flash-error'>
+      flash-error
+    </div>
   </div>
 ))
 .add('flash-success', () => (
-  <div className='flash flash-success'>
-    flash-success
+  <div className='p-4'>
+    <div className='flash flash-success'>
+      flash-success
+    </div>
   </div>
 ))
 .add('flash with action button', () => (
-  <div className="flash">
-    <button type="submit" className="btn btn-sm primary flash-action">Complete action</button>
-    Flash message with action here.
+  <div className='p-4'>
+    <div className="flash">
+      <button type="submit" className="btn btn-sm primary flash-action">Complete action</button>
+      Flash message with action here.
+    </div>
   </div>
 ))
 .add('flash-full', () => (
-  <div className="container-lg">
-    <div className="flash flash-full">
-      Full width flash message.
+  <div className='p-4'>
+    <div className="container-lg">
+      <div className="flash flash-full">
+        Full width flash message.
+      </div>
     </div>
   </div>
 ))
