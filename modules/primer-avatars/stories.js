e374740f0c3c752f74aa2094adb140a09a61cc2d
Joel Califa <jcalifa@gmail.com>
2017-11-02 15:27:31 -0400

    add first version of non-working right aligned modifier

--- modules/primer-avatars/stories.js
+++ modules/primer-avatars/stories.js
@@ -28,8 +28,8 @@ storiesOf('Avatars', module)
 ))
 .add('AvatarStack', () => (
   <div>
-    <div className='AvatarStack AvatarStack--3' aria-label='five avatars'>
-      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left'>
+    <div className='AvatarStack AvatarStack--3'>
+      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left' aria-label='five avatars'>
         <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
         <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
         <div className='avatar-more avatar'></div>
@@ -38,12 +38,29 @@ storiesOf('Avatars', module)
         <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
       </div>
     </div>
-    <div className='AvatarStack AvatarStack--2' aria-label='two avatars'>
-      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left'>
+    <div className='AvatarStack AvatarStack--2'>
+      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left' aria-label='two avatars'>
         <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
         <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
       </div>
     </div>
+    <div className='AvatarStack AvatarStack--3 AvatarStack--right'>
+      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left' aria-label='five avatars'>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+        <div className='avatar-more avatar'></div>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+      </div>
+    </div>
+    <div className='AvatarStack AvatarStack--2 AvatarStack--right'>
+      <div className='AvatarStack-body tooltipped tooltipped-se temp-tooltipped-align-left' aria-label='two avatars'>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+        <img className='avatar' alt='Uncle Cat' width='20' height='20' src='https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png'/>
+      </div>
+    </div>
+
   </div>
 ))
 .add('CircleBadge--small', () => (
