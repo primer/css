4c9bdf108103bac32798d646919f1dc703c20b08
Diana Mounter <broccolini@github.com>
2017-11-10 13:32:25 -0500

    move utilities stories into stories folder

--- modules/primer-utilities/stories/Borders.js
+++ modules/primer-utilities/stories/Borders.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-storiesOf('Utilities', module)
-.add('hover-grow', () => (
-  <div className='Box hover-grow p-4'>
-    .hover-grow
-  </div>
-))
+storiesOf('Border utilities', module)
 .add('circle', () => (
   <div className='border circle p-3' style={{width: '100px', height: '100px'}}>
     .circle
