4ab57c087a78b616819e3f7195747ae399b1e5a1
Diana Mounter <broccolini@github.com>
2017-11-15 11:53:55 -0500

    update stories and re-org

--- modules/primer-tooltips/stories.js
+++ modules/primer-tooltips/stories.js
@@ -30,38 +30,53 @@ storiesOf('Tooltips', module)
     </span>
   </div>
 ))
-.add('tootlipped-multiline', () => (
-  <div className='p-6'>
-    <span className='tooltipped tooltipped-multiline tooltipped-s border p-2' aria-label='This is the tooltip with multiple lines. This is the tooltip with multiple lines.'>
-      Tooltip with multiple lines
-    </span>
-  </div>
-))
-.add('tootlip no delay', () => (
-  <div className='p-6'>
-    <span className='tooltipped tooltipped-n tooltipped-no-delay border p-2' aria-label='This is a tooltip with no delay.'>
-      Tooltip no delay
-    </span>
-  </div>
-))
-.add('tooltip on button', () => (
-  <div className='p-6'>
-  <button className='btn btn-secondary tooltipped tooltipped-n' aria-label='This is the tooltip on a button.'>Button</button>
-  </div>
-))
 .add('tooltip aligned', () => (
   <div className='p-4'>
-    <span className='tooltipped tooltipped-ne tooltipped-align-left border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NE and aligned left.'>
+    <span className='tooltipped tooltipped-ne tooltipped-align-left-1 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NE and aligned left.'>
     Tooltip North East align left
     </span>
-    <span className='tooltipped tooltipped-se tooltipped-align-left border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SW and aigned left.'>
+    <span className='tooltipped tooltipped-ne tooltipped-align-left-2 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NE and aligned left.'>
+    Tooltip North East align left
+    </span>
+    <span className='tooltipped tooltipped-se tooltipped-align-left-1 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SW and aigned left.'>
     Tooltip South East align left
     </span>
-    <span className='tooltipped tooltipped-nw tooltipped-align-right border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NW and aligned right.'>
+    <span className='tooltipped tooltipped-se tooltipped-align-left-2 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SW and aigned left.'>
+    Tooltip South East align left
+    </span>
+    <span className='tooltipped tooltipped-nw tooltipped-align-right-1 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NW and aligned right.'>
       Tooltip North West align right
     </span>
-    <span className='tooltipped tooltipped-sw tooltipped-align-right border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SE and aligned right.'>
+    <span className='tooltipped tooltipped-nw tooltipped-align-right-2 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped NW and aligned right.'>
+      Tooltip North West align right
+    </span>
+    <span className='tooltipped tooltipped-sw tooltipped-align-right-1 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SE and aligned right.'>
+      Tooltip South West align right
+    </span>
+    <span className='tooltipped tooltipped-sw tooltipped-align-right-2 border p-2 mb-2 mr-2 float-left' aria-label='Tooltipped SE and aligned right.'>
       Tooltip South West align right
     </span>
   </div>
+  ))
+  .add('tootlipped-multiline', () => (
+    <div className='p-6'>
+      <span className='tooltipped tooltipped-multiline tooltipped-s border p-2 mr-2' aria-label='This is the tooltip with multiple lines. This is the tooltip with multiple lines.'>
+        Tooltip with multiple lines
+      </span>
+      <span className='tooltipped tooltipped-multiline tooltipped-sw tooltipped-align-right-2 border p-2' aria-label='This is the tooltip with multiple lines. This is the tooltip with multiple lines.'>
+        Tooltip with multiple lines
+      </span>
+    </div>
+  ))
+  .add('tootlip no delay', () => (
+    <div className='p-6'>
+      <span className='tooltipped tooltipped-n tooltipped-no-delay border p-2' aria-label='This is a tooltip with no delay.'>
+        Tooltip no delay
+      </span>
+    </div>
+  ))
+  .add('tooltip on button', () => (
+    <div className='p-6'>
+    <button className='btn btn-secondary tooltipped tooltipped-n' aria-label='This is the tooltip on a button.'>Button</button>
+    </div>
 ))
