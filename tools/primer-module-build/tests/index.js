5a0947f463b1596f23f397c7b0a05990d2cabd0b
Shawn Allen <shawn.allen@github.com>
2018-04-30 15:19:11 -0700

    moar tests

--- tools/primer-module-build/tests/index.js
+++ tools/primer-module-build/tests/index.js
@@ -1,9 +1,18 @@
-const test = require('ava')
 const build = require('../lib/build.js')
+const test = require('ava')
+const tempy = require('tempy')
+const {exists, remove} = require('fs-extra')
 const {join} = require('path')
 
 function fixture(...path) {
   return join(__dirname, 'fixtures', ...path)
+}
+
+function assertExists(t, filename) {
+  return exists(filename)
+    .then(exists => {
+      exists ? t.pass() : t.fail(`No such file: ${filename}`)
+    })
 }
 
 test('resolves npm-installed primer package', t => {
@@ -14,4 +23,11 @@ test('resolves npm-installed primer package', t => {
 test('resolves relative paths', t => {
   return build(fixture('relative.scss'), {})
     .then(() => t.pass())
+})
+
+test('writes build.css by default', t => {
+  const outputDir = tempy.directory()
+  const outputFile = join(outputDir, 'build.css')
+  return build(fixture('primer-package.scss'), {outputDir})
+    .then(() => assertExists(t, outputFile))
 })
