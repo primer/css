e97cc01250a9f8d44c91f53744d2dc21a6c527ea
muan <muan@github.com>
2018-06-02 01:44:02 +0200

    .details-expanded -> .details-overlay

--- modules/primer-utilities/stories/Details.js
+++ modules/primer-utilities/stories/Details.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 storiesOf('Details utilities', module)
-.add('details-expanded', () => (
-  <details className='details-expanded'>
+.add('details-overlay', () => (
+  <details className='details-overlay'>
     <summary className='btn'>More</summary>
     <div className='position-relative bg-white p-3 border' style={{zIndex: 100}}>Hidden text</div>
   </details>
 ))
-.add('details-expanded-dark', () => (
-  <details className='details-expanded details-expanded-dark'>
+.add('details-overlay-dark', () => (
+  <details className='details-overlay details-overlay-dark'>
     <summary className='btn'>More</summary>
     <div className='position-relative bg-white p-3 border' style={{zIndex: 100}}>Hidden text</div>
   </details>
