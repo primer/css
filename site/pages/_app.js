3a9f46c7580542cf408c24ff388de570ad2ca924
Shawn Allen <shawn.allen@github.com>
2018-10-22 14:27:48 -0700

    tidy up with tree diff test

--- site/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import App from 'next/app'
-import {nest} from 'recompose'
-import {BaseStyles} from '@primer/components'
-
-export default nest(App, BaseStyles)
--- site/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Box} from '@primer/components'
-
-export default () => (
-  <Box p={4}>
-    Hello, world!
-  </Box>
-)
