3af1845f124992692aeffa9fcfe5dc95ffd203c5
Shawn Allen <shawn.allen@github.com>
2018-11-08 09:58:56 -0800

    create classes.json at prepare time

---
+++ tools/stylelint-selector-no-utility/prepare.js
@@ -0,0 +1,24 @@
+const {join} = require('path')
+const {writeFile} = require('fs-extra')
+const utilities = require('primer-utilities')
+
+const match = require('./match')
+const classes = utilities.cssstats.selectors.values.reduce((set, selector) => {
+  const matches = match(selector)
+  if (matches) {
+    for (const klass of Array.from(matches)) {
+      set.add(klass)
+    }
+  }
+  return set
+}, new Set())
+
+const data = JSON.stringify(Array.from(classes), null, 2)
+const filename = join(__dirname, 'classes.json')
+
+writeFile(filename, data, 'utf8')
+  .then(() => console.warn(`wrote ${classes.size} classes to: ${filename}`))
+  .catch(error => {
+    console.error(error)
+    process.exitCode = 1
+  })
