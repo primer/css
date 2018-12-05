7448568234c542842bf9007f5f70ef044aa2b3f6
Shawn Allen <shawn.allen@github.com>
2018-12-04 22:03:25 -0800

    build full page list, don't exit(1) on diff

--- docs/test.sh
+++ docs/test.sh
@@ -4,12 +4,19 @@ cwd=`pwd`
 
 if [[ ! -e before.txt ]]; then
     pushd ../../../github/styleguide/src/_primer > /dev/null
-    find . -name '*.md' > $cwd/before.txt
+    find . -name '*.md' | sort -u > $cwd/tmp.txt
     popd > /dev/null
+    pushd ../../../github/styleguide/src/primer > /dev/null
+    find . -name '*.md' | sort -u >> $cwd/tmp.txt
+    popd > /dev/null
+    cat tmp.txt | sort -u > before.txt
+    rm tmp.txt
 fi
 
 pushd pages/css > /dev/null
-find . -name '*.md' > $cwd/after.txt
+find . -name '*.md' | sort -u > $cwd/after.txt
 popd > /dev/null
 
-diff before.txt after.txt
+diff before.txt after.txt || {
+  echo "There was a diff, but it's probably not a big deal?"
+}
