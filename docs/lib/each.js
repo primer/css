abc4ae58ffac6618a2851224997ed133fdd834fd
Shawn Allen <shawn.allen@github.com>
2018-12-04 22:01:19 -0800

    remove unused lib/*.js

--- docs/lib/each.js
+++
@@ -1,8 +0,0 @@
-module.exports = function each(fn) {
-  return (files, metal, done) => {
-    for (const [path, file] of Object.entries(files)) {
-      fn(path, file, files, metal)
-    }
-    done()
-  }
-}
