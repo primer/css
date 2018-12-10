d9bd354412b0be0683449e8c7f06d6b884e8313c
Shawn Allen <shawn.allen@github.com>
2018-12-10 08:05:22 -0800

    add ClipboardCopy component

---
+++ docs/src/ClipboardCopy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+
+const ClipboardCopy = React.createFactory('clipboard-copy')
+
+export default dynamic(() => {
+  return import('clipboard-copy-element').then(() => ClipboardCopy)
+}, {
+  ssr: false
+})
