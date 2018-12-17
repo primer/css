7a71728272ccf477ed8071e26ed3e421b8d3a190
Shawn Allen <shawn.allen@github.com>
2018-12-17 15:04:12 -0800

    add details-dialog-element

---
+++ docs/src/DetailsDialog.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+
+export default dynamic(() => {
+  return import('details-dialog-element').then(() => React.createFactory('details-dialog'))
+}, {ssr: false})
