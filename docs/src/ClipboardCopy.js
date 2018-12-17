821f92cdbd2fe4e1ac2fbbeaf328f967d3342095
Shawn Allen <shawn.allen@github.com>
2018-12-17 14:43:38 -0800

    use our own clipboard copy logic /shrug

--- docs/src/ClipboardCopy.js
+++ docs/src/ClipboardCopy.js
@@ -1,13 +1,45 @@
 import React from 'react'
-import dynamic from 'next/dynamic'
+import {findDOMNode} from 'react-dom'
+import {Button} from '@primer/components'
+import Octicon, {Clippy} from '@githubprimer/octicons-react'
 
-const ClipboardCopy = React.createFactory('clipboard-copy')
+export default class ClipboardCopy extends React.Component {
+  copy() {
+    const {value = ''} = this.props
+    const {clipboard} = window.navigator
+    const done = () => {
+      findDOMNode(this).dispatchEvent(new CustomEvent('copy', {bubbles: false}))
+    }
+    if (clipboard) {
+      return clipboard.writeText(value).then(done)
+    } else if (!document.body) {
+      return
+    } else {
+      var node = document.createElement('pre')
+      node.style.width = '1px'
+      node.style.height = '1px'
+      node.style.position = 'fixed'
+      node.style.top = '5px'
+      node.textContent = value
 
-export default dynamic(
-  () => {
-    return import('clipboard-copy-element').then(() => ClipboardCopy)
-  },
-  {
-    ssr: false
+      document.body.appendChild(node)
+      selection.removeAllRanges()
+      const range = document.createRange()
+      range.selectNodeContents(node)
+      selection.addRange(range)
+      document.execCommand('copy')
+      selection.removeAllRanges()
+      document.body.removeChild(node)
+    }
   }
-)
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const {value = '', ...rest} = this.props
+    return (
+      <Button onClick={() => this.copy()} type="button" {...rest}>
+        <Octicon icon={Clippy} />
+      </Button>
+    )
+  }
+}
