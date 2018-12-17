aa77f0182da93eb2a78dd99dfd0065344eb87362
Shawn Allen <shawn.allen@github.com>
2018-12-17 15:05:58 -0800

    lint

--- docs/src/DetailsDialog.js
+++ docs/src/DetailsDialog.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 
-export default dynamic(() => {
-  return import('details-dialog-element').then(() => React.createFactory('details-dialog'))
-}, {ssr: false})
+export default dynamic(
+  () => {
+    return import('details-dialog-element').then(() => React.createFactory('details-dialog'))
+  },
+  {ssr: false}
+)
