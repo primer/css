03fa9d5034a87ed55e0691aad614caae79c80999
Catherine Bui <gladwearefriends@github.com>
2019-01-03 11:52:29 -0800

    update stories.js

--- modules/primer-marketing-buttons/stories.js
+++ modules/primer-marketing-buttons/stories.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import storiesFromMarkdown from '../../.storybook/lib/storiesFromMarkdown'
 
-storiesOf('Marketing buttons', module)
-  .add('Button colors and outlined', () => (
-    <div className='p-4'>
-      <button class="btn-mktg mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-outline-mktg mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-primary-mktg mr-3" type="button">Sign up</button>
-    </div>
-  ))
-  .add('Button sizes', () => (
-    <div className='p-4'>
-      <button class="btn-mktg mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-outline-mktg  mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-primary-mktg  mr-3" type="button">Sign up</button>
-      <button class="btn-mktg btn-large-mktg  mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-outline-mktg btn-large-mktg  mr-3" type="button">Learn more</button>
-      <button class="btn-mktg btn-primary-mktg btn-large-mktg  mr-3" type="button">Sign up</button>
-    </div>
-  ))
+const stories = storiesOf('Marketing buttons', module)
+
+for (const {title, story} of storiesFromMarkdown(require.context('.', true, /\.md$/))) {
+  stories.add(title, story)
+}
