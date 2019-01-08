f8c6412e4895e7bd1ce951b1db31bdd6020afb9e
Shawn Allen <shawn.allen@github.com>
2019-01-07 21:57:42 -0800

    nix duplicate commit status

--- modules/primer/scripts/postpublish.js
+++ modules/primer/scripts/postpublish.js
@@ -7,6 +7,3 @@ const version = require('../package.json').version
 
 const path = join(root, 'primer-version.txt')
 writeFileSync(path, version, 'utf8')
-
-const cwd = process.cwd()
-execa.sync(join(root, 'script/notify'), ['success'], {cwd, stdio: 'inherit'})
