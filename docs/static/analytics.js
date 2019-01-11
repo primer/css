c71b51f18fd235f080e477edcfe513642be65309
Shawn Allen <shawn.allen@github.com>
2019-01-11 10:16:56 -0800

    update GA UA from @jonrohan

--- docs/static/analytics.js
+++ docs/static/analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-// gtag('config', '/* FIXME */');
+gtag('config', 'UA-126681523-2');
