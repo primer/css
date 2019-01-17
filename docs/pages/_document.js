78d0e46b1f43bf0ff70ba8949ce1cea4a57a9281
Shawn Allen <shawn.allen@github.com>
2019-01-16 22:46:35 -0800

    lint pages/_document.js

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Document, {Head, Main, NextScript} from 'next/document'
 import {ServerStyleSheet} from 'styled-components'
-import {config, getAssetPath, CommonStyles, CommonScripts} from '../src/utils'
+import {getAssetPath, CommonStyles, CommonScripts} from '../src/utils'
 
 export default class MyDocument extends Document {
   static getInitialProps({renderPage}) {
