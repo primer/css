ed34ad1d3f703a07c54bf64f9177ad5456f9d8da
Shawn Allen <shawn.allen@github.com>
2019-02-19 11:39:42 -0800

    chore: lint

--- docs/redirect.js
+++ docs/redirect.js
@@ -30,7 +30,10 @@ export default function redirect(uri, status = 303) {
 }
 
 export function redirectTrailingSlash(context, status = 301) {
-  const {req: {url}, res} = context
+  const {
+    req: {url},
+    res
+  } = context
   if (url.endsWith('/')) {
     const withoutSlash = url.substr(0, url.length - 1)
     res.writeHead(status, {Location: withoutSlash})
