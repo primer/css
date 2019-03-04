88b890d961ef1d39fa7832b0654a4d25a66120f4
Shawn Allen <shawn.allen@github.com>
2019-03-04 10:22:44 -0800

    fix(storybook): use the top-level postcss config

--- .storybook/postcss.config.js
+++ .storybook/postcss.config.js
@@ -1,8 +1,1 @@
-// FIXME this should go away when we merge and release:
-// <https://github.com/primer/primer-module-build/pull/19>
-const plugins = require("primer-module-build/lib/.postcss.json")
-delete plugins.use
-
-module.exports = {
-  plugins,
-}
+module.exports = require('../postcss.config')
