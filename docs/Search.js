04d02f276e5495dad05891b9b089c610ad23f483
emplums <emplums@github.com>
2019-03-20 16:56:27 -0700

    margnial progress on search styles

--- docs/Search.js
+++ docs/Search.js
@@ -1,9 +1,11 @@
-import { Component } from 'react'
+import React, {useState} from 'react'
 import lunr from 'lunr'
+import {TextInput, Absolute, BorderBox, Box} from '@primer/components'
+import Link from './Link'
 import documents from '../searchIndex'
 
-class Search extends Component {
-  idx = lunr(function () {
+function Search() {
+  const idx = lunr(function () {
     this.ref('path')
     this.field('title')
     this.field('content')
@@ -13,15 +15,45 @@ class Search extends Component {
     }, this)
   })
 
-  onClick = () => {
-    console.log(this.idx.search("alerts"))
+  const [results, setResults] = useState([])
+  const [value, setValue] = useState("")
+  const [open, setOpen] = useState(false)
+
+  function openMenu() {
+    if (!open) {
+      setOpen(true)
+      document.addEventListener('click', closeMenu);
+    }
   }
 
-  render() {
-    return (
-      <button onClick={this.onClick}>search!</button>
-    )
+  function closeMenu() {
+    setOpen(false)
+    document.removeEventListener('click', closeMenu);
   }
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+    setResults(idx.search(e.target.value))
+    openMenu()
+  }
+
+  const renderResults = () => {
+    return results.map((result) => {
+      const doc = documents[result.ref]
+      return (
+        <Box><Link href={`/css/${doc.path}`}>{doc.title}</Link></Box>
+      )
+    })
+  }
+
+  return (
+    <Box>
+      <TextInput aria-label="search docs" onChange={e => onChange(e)}/>
+      <BorderBox display={open ? 'block' : 'none'} p={2} is={Absolute} borderRadius={1} bg='white'>
+        {renderResults()}
+      </BorderBox>
+    </Box>
+  )
 }
 
 export default Search
