6b065a0284aa3994b2178159178b3cc08810761a
Emily Plummer <emplums@github.com>
2019-04-25 13:19:02 -0700

    add edit link

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import React from 'react'
 import App, {Container} from 'next/app'
 import {MDXProvider} from '@mdx-js/tag'
 import Head from 'next/head'
-import {BaseStyles, BorderBox, Box, Flex, theme} from '@primer/components'
+import Octicon, {iconsByName, Pencil} from '@githubprimer/octicons-react'
+import {BaseStyles, Link, Text, StyledOcticon, BorderBox, Box, Flex, theme} from '@primer/components'
 import {PackageHeader} from '../docs/components'
 import {Header, MarkdownHeading, JumpNav, Section, Router, RouteMatch, SectionLink, SideNav} from '@primer/blueprints'
 import {NavList} from '@primer/blueprints/dist/next-components-index.umd.js'
@@ -10,8 +11,11 @@ import getComponents from '../docs/markdown'
 import documents from '../searchIndex'
 import {config, requirePage, rootPage} from '../docs/utils'
 import {CONTENT_MAX_WIDTH} from '../docs/constants'
+import {repository} from '../package.json'
 
 import '../src/index.scss'
+
+const DocLink = props => <Link nounderline {...props} />
 
 export default class MyApp extends App {
   static async getInitialProps({Component, ctx}) {
@@ -30,6 +34,8 @@ export default class MyApp extends App {
     const {Component, page} = this.props
     const node = rootPage.first(node => node.path === pathname) || {}
     const {file, meta = {}} = node || {}
+    const isIndex = file.includes('index')
+    const editLinkBase = `${repository}/edit/master/pages`
     const components = getComponents(node)
 
     const Hero = file ? requirePage(file).Hero : null
@@ -68,6 +74,17 @@ export default class MyApp extends App {
                       <summary>Metadata</summary>
                       <pre>{JSON.stringify(meta, null, 2)}</pre>
                     </details>
+                  )}
+                  {pathname && (
+                    <Box color="gray.5" borderColor="gray.2" borderTop={1} my={6} pt={2}>
+                      <Text mr={2}>
+                        <Octicon icon={Pencil} />
+                      </Text>
+                      <DocLink muted href={`${editLinkBase}${pathname}${isIndex ? '/index' : ''}.md`}>
+                        Edit this page
+                      </DocLink>{' '}
+                      on GitHub
+                    </Box>
                   )}
                 </div>
               </Box>
