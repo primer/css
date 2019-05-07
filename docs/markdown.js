3da919a71231d2614bec84f6ac6d2cc25933d20d
Emily Plummer <emplums@github.com>
2019-05-07 14:53:38 -0700

    code style tweaks

--- docs/markdown.js
+++ docs/markdown.js
@@ -9,18 +9,18 @@ export const H3 = props => <MarkdownHeading as="h3" {...props} />
 export const H4 = props => <MarkdownHeading as="h4" {...props} />
 export const H5 = props => <MarkdownHeading as="h5" {...props} />
 
-export default function getComponents() {
-  return {
-    h1: H1,
-    h2: H2,
-    h3: H3,
-    h4: H4,
-    h5: H5,
-    // render links with our component
-    a: Link,
-    // render code blocks with our wrapper around react-live
-    code: CodeExample,
-    // "unwrap" <pre> elements around <code> blocks
-    pre: props => props.children
-  }
+const components = {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  // render links with our component
+  a: Link,
+  // render code blocks with our wrapper around react-live
+  code: CodeExample,
+  // "unwrap" <pre> elements around <code> blocks
+  pre: props => props.children
 }
+
+export default components
