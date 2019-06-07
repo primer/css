ed7d0bf1772801729588c81155db1d949a884ac2
Shawn Allen <shawn.allen@github.com>
2019-06-07 11:01:59 -0700

    use @primer/blueprints/configure

--- next.config.js
+++ next.config.js
@@ -1,21 +1,18 @@
-const {join, resolve} = require('path')
-const withNextPages = require('@primer/next-pages/plugin')
+const {join, dirname, resolve} = require('path')
 const withSass = require('@zeit/next-sass')
-const configure = require('./lib/config')
+const configure = require('@primer/blueprints/configure')
 
 module.exports = configure(
-  withNextPages(
-    withSass({
-      target: 'serverless',
-      env: {
-        GOOGLE_ANALYTICS_UA: 'UA-126681523-1'
-      },
-      sassLoaderOptions: {
-        includePaths: [
-          resolve(__dirname, '../modules'),
-          resolve(__dirname, 'node_modules')
-        ]
-      }
-    })
-  )
+  withSass({
+    target: 'serverless',
+    env: {
+      GOOGLE_ANALYTICS_UA: 'UA-126681523-1'
+    },
+    sassLoaderOptions: {
+      includePaths: [
+        resolve(__dirname, '../modules'),
+        resolve(__dirname, 'node_modules')
+      ]
+    }
+  })
 )
