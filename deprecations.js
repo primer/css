5492a3ab0589a474e44aba31eef89e7ca2bab7f2
Shawn Allen <shawn.allen@github.com>
2019-09-10 13:32:11 -0700

    chore: lint deprecations.js

--- deprecations.js
+++ deprecations.js
@@ -19,8 +19,12 @@ const versionDeprecations = {
     },
     {
       selectors: [
-        '.container', '.container::before', '.container::after',
-        '.columns', '.columns::before', '.columns::after',
+        '.container',
+        '.container::before',
+        '.container::after',
+        '.columns',
+        '.columns::before',
+        '.columns::after',
         '.column',
         '.one-third',
         '.two-thirds',
