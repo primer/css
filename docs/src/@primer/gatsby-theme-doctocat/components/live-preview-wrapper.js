4726ce2515ec7c5a4db625bad79f230528d05c51
Emily Plummer <emplums@github.com>
2019-09-12 13:04:46 -0700

    add iframe to live preview wrapper

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import primerStyles from '!!raw-loader!postcss-loader!../../../../../src/index.scss'
+import {Frame} from '@primer/gatsby-theme-doctocat'
 
 function LivePreviewWrapper({children}) {
   return (
-    <>
+    <Frame>
       <link rel="stylesheet" href="https://github.com/site/assets/styleguide.css" />
       <style>{primerStyles}</style>
       <div className="p-3">{children}</div>
-    </>
+    </Frame>
   )
 }
 
