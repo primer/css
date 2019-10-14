08fe118c758c095ce2825a223308f9d3aa96f9ce
Shawn Allen <shawn.allen@github.com>
2019-10-14 16:24:34 -0700

    add TODO(version) tests

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,11 @@
+const currentVersion = process.env.PRIMER_VERSION || require('./package.json').version
+
 module.exports = {
   extends: ['stylelint-config-primer'],
+  plugins: ['./lib/stylelint-todo'],
   syntax: 'scss',
   rules: {
-    'primer/no-override': false
+    'primer/no-override': false,
+    'primer-css/TODO': [true, {currentVersion, severity: 'error'}]
   }
 }
