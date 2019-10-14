ceb67a351cac31adca799348da4f1bbfa1b9ede5
Shawn Allen <shawn.allen@github.com>
2019-10-14 16:31:51 -0700

    chore: lint

--- script/test-todos.js
+++ script/test-todos.js
@@ -5,21 +5,20 @@ const {red} = require('colorette')
 const ruleName = 'primer-css/TODO'
 const cwd = process.cwd()
 
-stylelint
-  .lint({files: 'src/**/*.scss'})
-  .then(data => {
-    let fail = false
-    for (const {source, warnings} of data.results) {
-      if (warnings.some(w => w.rule === ruleName)) {
-        console.warn('\n' + source.substr(cwd.length + 1))
-      }
-      for (const warning of warnings) {
-        if (warning.rule === ruleName) {
-          console.warn(`${red('✖')} ${warning.text}`)
-          fail = true
-        }
+stylelint.lint({files: 'src/**/*.scss'}).then(data => {
+  let fail = false
+  for (const {source, warnings} of data.results) {
+    if (warnings.some(w => w.rule === ruleName)) {
+      // eslint-disable-next-line prefer-template
+      console.warn('\n' + source.substr(cwd.length + 1))
+    }
+    for (const warning of warnings) {
+      if (warning.rule === ruleName) {
+        console.warn(`${red('✖')} ${warning.text}`)
+        fail = true
       }
     }
+  }
 
-    process.exit(fail ? 1 : 0)
-  })
+  process.exit(fail ? 1 : 0)
+})
