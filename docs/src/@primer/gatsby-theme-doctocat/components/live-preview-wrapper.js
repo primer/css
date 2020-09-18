5038b88d06dadd1df5edb2faec2ab9ae7f536cdb
Cole Bemis <colebemis@github.com>
2020-09-18 10:44:14 -0700

    Add color mode toggle to code examples

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -3,11 +3,18 @@ import primerStyles from '!!raw-loader!postcss-loader!../../../../../src/index.s
 import {Frame} from '@primer/gatsby-theme-doctocat'
 
 function LivePreviewWrapper({children}) {
+  const [colorMode, setColorMode] = React.useState('light')
+
   return (
     <Frame>
       <link rel="stylesheet" href="https://github.com/site/assets/styleguide.css" />
       <style>{primerStyles}</style>
-      <div className="frame-example p-3">{children}</div>
+      <div data-color-mode={colorMode}>
+        <button className="btn btn-sm" onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
+          Toggle
+        </button>
+        <div className="frame-example p-3">{children}</div>
+      </div>
     </Frame>
   )
 }
