9f99bc5dc6d900ab8e4522cdd676f5408dcbe8de
Cole Bemis <colebemis@github.com>
2020-09-21 13:22:28 -0700

    Add color mode toggle to live code examples (#1171)
    * Add color mode toggle to code examples
    
    * Style color mode toggle

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,13 +1,28 @@
-import React from 'react'
 import primerStyles from '!!raw-loader!postcss-loader!../../../../../src/index.scss'
+import {Flex} from '@primer/components'
 import {Frame} from '@primer/gatsby-theme-doctocat'
+import {MoonIcon, SunIcon} from '@primer/octicons-react'
+import React from 'react'
 
 function LivePreviewWrapper({children}) {
+  const [colorMode, setColorMode] = React.useState('light')
+
   return (
     <Frame>
       <link rel="stylesheet" href="https://github.com/site/assets/styleguide.css" />
       <style>{primerStyles}</style>
-      <div className="frame-example p-3">{children}</div>
+      <div data-color-mode={colorMode}>
+        <Flex pt={2} px={2} justifyContent="flex-end">
+          <button
+            className="btn"
+            aria-label={colorMode === 'light' ? 'Activate dark mode' : 'Activate light mode'}
+            onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+          >
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          </button>
+        </Flex>
+        <div className="frame-example p-3">{children}</div>
+      </div>
     </Frame>
   )
 }
