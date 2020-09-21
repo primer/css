dd0c9a909da1ee2170dcddb170e3b1108f27f5a4
Cole Bemis <colebemis@github.com>
2020-09-21 13:12:50 -0700

    Style color mode toggle

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,6 +1,8 @@
-import React from 'react'
 import primerStyles from '!!raw-loader!postcss-loader!../../../../../src/index.scss'
+import {Flex} from '@primer/components'
 import {Frame} from '@primer/gatsby-theme-doctocat'
+import {MoonIcon, SunIcon} from '@primer/octicons-react'
+import React from 'react'
 
 function LivePreviewWrapper({children}) {
   const [colorMode, setColorMode] = React.useState('light')
@@ -10,9 +12,15 @@ function LivePreviewWrapper({children}) {
       <link rel="stylesheet" href="https://github.com/site/assets/styleguide.css" />
       <style>{primerStyles}</style>
       <div data-color-mode={colorMode}>
-        <button className="btn btn-sm" onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
-          Toggle
-        </button>
+        <Flex pt={2} px={2} justifyContent="flex-end">
+          <button
+            className="btn"
+            aria-label={colorMode === 'light' ? 'Activate dark mode' : 'Activate light mode'}
+            onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+          >
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          </button>
+        </Flex>
         <div className="frame-example p-3">{children}</div>
       </div>
     </Frame>
