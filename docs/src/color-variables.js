a9b1e26d78d18c7021381b7c501194bcd209bdf5
Michelle Tilley <michelle@michelletilley.net>
2020-09-24 11:21:40 -0700

    Fix color pages

--- docs/src/color-variables.js
+++ docs/src/color-variables.js
@@ -1,81 +1,43 @@
 import titleCase from 'title-case'
-import primerColors from 'primer-colors'
-
-import colorSystemSCSS from '!!raw-loader!../../src/support/variables/color-system.scss'
-import colorVariablesSCSS from '!!raw-loader!../../src/support/variables/colors.scss'
-
-const variables = {}
-
-parseSCSSVariables(colorSystemSCSS, variables)
-parseSCSSVariables(colorVariablesSCSS, variables)
+import lightModeColors from '@primer/primitives/dist/js/colors/light'
 
 // XXX we don't necessarily define them in this order in primer-colors,
 // so we define an array here just to be safe
 const gradientHues = ['gray', 'blue', 'green', 'purple', 'yellow', 'orange', 'red', 'pink']
 
-const colors = {
-  ...primerColors,
-  pink: Object.keys(variables)
-    .filter(key => key.startsWith('pink-'))
-    .sort()
-    .map(key => variables[key])
-}
-
-const aliases = {}
+const colors = lightModeColors.scale
 
 const palettes = gradientHues.map(name => {
   return {
     name,
     title: titleCase(name),
-    value: variables[name] || colors[name][5],
+    value: colors[name][5],
     values: colors[name].map((value, index) => ({
       value,
       index,
-      variable: `${name}-${index}00`,
-      slug: `${name}-${index}`,
-      aliases: (aliases[value] = {})
+      variable: `--color-scale-${name}-${index}`,
+      slug: `color-scale-${name}-${index}`,
+      aliases: {}
     }))
   }
 })
 
-for (const key of Object.keys(variables)) {
-  const match = key.match(/^(bg|text|border)-(\w+)(-(dark|light))?$/)
-  const value = variables[key]
-  if (match && aliases[value]) {
-    // eslint-disable-next-line no-unused-vars
-    const [_, type, name, suffix] = match
-    aliases[value][type] = key
-  }
-}
-
-export {colors, gradientHues, palettes, getPaletteByName, variables}
+export {colors, gradientHues, palettes, getPaletteByName}
 
 export const allColors = palettes.reduce((all, {values}) => all.concat(values), [])
 
-export const borders = Object.keys(variables)
-  .filter(key => key.startsWith('border-') && !variables[key].includes('$'))
-  .sort()
-  .map(key => ({
-    variable: key,
-    value: variables[key],
-    slug: key,
-    aliases: {border: key}
-  }))
+// TODO: fix the borders code
+//
+// export const borders = Object.keys(variables)
+//   .filter(key => key.startsWith('border-') && !variables[key].includes('$'))
+//   .sort()
+//   .map(key => ({
+//     variable: key,
+//     value: variables[key],
+//     slug: key,
+//     aliases: {border: key}
+//   }))
 
 function getPaletteByName(name) {
   return palettes.find(palette => palette.name === name)
-}
-
-function parseSCSSVariables(scssString, variables = {}) {
-  const variablePattern = /\$([-\w]+):\s*(.+)( !default);/g
-  let match
-  do {
-    match = variablePattern.exec(scssString)
-    if (match) {
-      // eslint-disable-next-line no-unused-vars
-      const [_, name, value] = match
-      variables[name] = value.startsWith('$') ? variables[value.substr(1)] : value
-    }
-  } while (match)
-  return variables
 }
