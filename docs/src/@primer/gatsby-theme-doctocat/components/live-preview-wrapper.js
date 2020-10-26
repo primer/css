eca4322c6d7148217dd6a0d21afccbf6fef1ef3c
Michelle Tilley <michelle@michelletilley.net>
2020-10-26 10:56:27 -0700

    Make color mode toggle global

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -4,8 +4,28 @@ import {Frame} from '@primer/gatsby-theme-doctocat'
 import {MoonIcon, SunIcon} from '@primer/octicons-react'
 import React from 'react'
 
+class ColorModeChangeEvent extends CustomEvent {
+  constructor(mode) {
+    super('color-mode-change', {detail: mode})
+  }
+}
+
 function LivePreviewWrapper({children}) {
-  const [colorMode, setColorMode] = React.useState('light')
+  const [colorMode, setColorModeState] = React.useState('light')
+
+  const setColorMode = mode => {
+    setColorModeState(mode)
+    window.dispatchEvent(new ColorModeChangeEvent(mode))
+  }
+
+  React.useEffect(() => {
+    const callback = e => setColorModeState(e.detail)
+    window.addEventListener('color-mode-change', callback)
+
+    return () => {
+      window.removeEventListener('color-mode-change', callback)
+    }
+  }, [])
 
   return (
     <Frame>
