786f35d45694113f687659b1b122095e6dc5d2c2
Jon Rohan <yes@jonrohan.codes>
2021-03-31 23:11:55 -0700

    writing deprecation tests

--- __tests__/css.test.js
+++ __tests__/css.test.js
@@ -1,8 +1,56 @@
-const p = require('@primer/css/deprecations')
-const {currentVersionDeprecations} = require('./utils/css')
+const {
+  getCurrentVersion,
+  getDeprecatedSelectors,
+  getDeprecatedVariables,
+  getSelectorDiff,
+  getVariableDiff
+} = require('./utils/css')
+const semver = require('semver')
 
-describe('deprications', () => {
-  it('g', () => {
-    console.log(currentVersionDeprecations())
+let selectorsDiff, variablesDiff, version
+
+beforeAll(async () => {
+  selectorsDiff = getSelectorDiff()
+  variablesDiff = getVariableDiff()
+  version = getCurrentVersion()
+})
+
+describe('deprecations', () => {
+  it('A selector was marked as deprecated but not removed from the codebase', () => {
+    const removed = selectorsDiff['removed']
+    const deprecations = getDeprecatedSelectors(version)
+    if (deprecations.length) {
+      // Selectors were marked to be deprecated in this version,
+      // but were not removed from the codebase. Please remove these selectors.
+      expect(deprecations.sort()).toEqual(removed.sort())
+    }
+  })
+
+  it('A selector was removed from the codebase and added to upcoming major release deprecations file.', () => {
+    const removedSelectors = selectorsDiff['removed']
+    const nextMajor = semver.inc(version, 'major')
+    const deprecations = getDeprecatedSelectors(nextMajor)
+    // Some classes were removed from the codebase, but not found
+    // in the next upcoming major release deprecation.json
+    expect(deprecations).toEqual(expect.arrayContaining(removedSelectors))
+  })
+
+  it('A variable was marked as deprecated but not removed from the codebase', () => {
+    const removed = variablesDiff.removed
+    const deprecations = getDeprecatedVariables(version)
+    if (deprecations.length) {
+      // Variables were marked to be deprecated in this version,
+      // but were not removed from the codebase. Please remove these variables.
+      expect(deprecations.sort()).toEqual(removed.sort())
+    }
+  })
+
+  it('A variable was removed from the codebase and added to upcoming major release deprecations file.', () => {
+    const removed = variablesDiff.removed
+    const nextMajor = semver.inc(version, 'major')
+    const deprecations = getDeprecatedVariables(nextMajor)
+    // Some variables were removed from the codebase, but not found
+    // in the next upcoming major release deprecation.json
+    expect(deprecations).toEqual(expect.arrayContaining(removed))
   })
 })
--- __tests__/utils/css.js
+++ __tests__/utils/css.js
@@ -1,11 +1,72 @@
 const {join} = require('path')
+const currentPath = join(__dirname, '../../')
+const lastPath = join(__dirname, '../../tmp/node_modules/@primer/css')
 
-const distPath = join(__dirname, '../../dist')
+function diffLists(before, after) {
+  const added = after.filter(value => !before.includes(value))
+  const removed = before.filter(value => !after.includes(value))
+  return {
+    changed: added.length + removed.length,
+    added,
+    removed
+  }
+}
 
-function currentVersionDeprecations() {
-  require(join(distPath, './deprecations.json'))
+function getSelectors(versionPath) {
+  const stats = require(join(versionPath, './stats/primer.json'))
+  return stats.selectors.values
+}
+
+function getVariables(versionPath) {
+  const variables = require(join(versionPath, './variables.json'))
+  return Object.keys(variables)
+}
+
+function getCurrentVersion() {
+  const pkg = require(join(currentPath, './package.json'))
+  return pkg.version
+}
+
+function getDeprecatedSelectors(version) {
+  let deprecations = require(join(currentPath, './dist/deprecations.json'))
+  deprecations = deprecations.versions[version] || []
+  return deprecations.reduce((list, deprecation) => list.concat(deprecation.selectors), []).filter(v => v)
+}
+
+function getDeprecatedVariables(version) {
+  let deprecations = require(join(currentPath, './dist/deprecations.json'))
+  deprecations = deprecations.versions[version] || []
+  return deprecations.reduce((list, deprecation) => list.concat(deprecation.variables), []).filter(v => v)
+}
+
+function currentVersionSelectors() {
+  return getSelectors(join(currentPath, './dist'))
+}
+
+function currentVersionVariables() {
+  return getVariables(join(currentPath, './dist'))
+}
+
+function lastVersionSelectors() {
+  return getSelectors(join(lastPath, './dist'))
+}
+
+function lastVersionVariables() {
+  return getVariables(join(lastPath, './dist'))
+}
+
+function getSelectorDiff() {
+  return diffLists(lastVersionSelectors(), currentVersionSelectors())
+}
+
+function getVariableDiff() {
+  return diffLists(lastVersionVariables(), currentVersionVariables())
 }
 
 module.exports = {
-  currentVersionDeprecations
+  getCurrentVersion,
+  getDeprecatedSelectors,
+  getDeprecatedVariables,
+  getSelectorDiff,
+  getVariableDiff
 }
