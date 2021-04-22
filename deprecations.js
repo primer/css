45931a81aecece4c26cfedccd323e5fa636dfef2
Tobias Ahlin Bjerrome <hello@tobiasahlin.com>
2021-04-22 17:47:18 +0200

    Add deprecation message for btn-outline-blue

--- deprecations.js
+++ deprecations.js
@@ -8,6 +8,10 @@ const versionDeprecations = {
     {
       selectors: [':-ms-input-placeholder'],
       message: 'Browserslist update to match github has removed the need for this pseudoselector'
+    },
+    {
+      selectors: ['.btn-outline-blue'],
+      message: `This selector is not available in Primer CSS 16.0.0. Please refer to the documentation.`
     }
   ],
   '16.0.0': [
