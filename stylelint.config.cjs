b5319afa03bd03361959ce5558f0b4f45670412c
Jon Rohan <yes@jonrohan.codes>
2021-09-29 14:26:35 -0700

    Put back these configs

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -6,6 +6,11 @@ module.exports = {
   rules: {
     'scss/dollar-variable-default': [true, {ignore: 'local'}],
     'primer/no-override': false,
+    'primer/colors': true,
+    'primer/borders': true,
+    'primer/spacing': true,
+    'primer/typography': true,
+    'primer/box-shadow': true,
     'primer/no-undefined-vars': [true, {files: 'node_modules/@primer/primitives/dist/scss/colors*/*.scss'}]
   }
 }
