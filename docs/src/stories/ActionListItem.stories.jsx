26f4464132a7d748374b23a75f6a10d5c9c405f3
langermank <langermank@github.com>
2021-10-05 13:08:48 -0700

    one grid option

--- docs/src/stories/ActionListItem.stories.jsx
+++ docs/src/stories/ActionListItem.stories.jsx
@@ -22,7 +22,7 @@ export default {
 export const Playground = args => (
   <>
     <li className="actionList-item actionList-item--active">
-      <div className="actionList-item-content">
+      <div className="actionList-item-content actionList-item--trailingVisual">
         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
           <path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path>
           <path
@@ -30,14 +30,14 @@ export const Playground = args => (
             d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"
           ></path>
         </svg> */}
-        <img
-          class="avatar avatar-small"
+        {/* <img
+          class="avatar avatar-small leadingVisual"
           alt="jonrohan"
           src="https://github.com/jonrohan.png?v=3&s=40"
           width="20"
           height="20"
           style={{borderRadius: '100% '}}
-        />
+        /> */}
         <span className="actionList-item-label">Item with leading visual</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
           <path
@@ -48,15 +48,15 @@ export const Playground = args => (
       </div>
     </li>
     <li className="actionList-item">
-      <div className="actionList-item-content actionList-item--sizeMedium">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+      <div className="actionList-item-content actionList-item--leadingVisual actionList-item--trailingVisual actionList-item--sizeMedium">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" class="leadingVisual">
           <path
             fill-rule="evenodd"
             d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
           ></path>
         </svg>
         <span className="actionList-item-label">Item with leading visual</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" class="trailingVisual">
           <path
             fill-rule="evenodd"
             d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
@@ -96,7 +96,7 @@ Playground.decorators = [
 export const TextOnly = args => (
   <li className="actionList-item">
     <span className="actionList-item-content">
-      <span className="actionList-item-label">Item with leading visual</span>
+      <span className="actionList-item-label">Default text only item</span>
     </span>
   </li>
 )
@@ -112,9 +112,11 @@ TextOnly.decorators = [
 
 export const SizeMedium = args => (
   <>
-    <ul className="actionList actionList--sizeMedium">
-      <li className="actionList-item actionList-item--sizeMedium">
-        <span className="actionList-item-content">Medium item size</span>
+    <ul className="actionList">
+      <li className="actionList-item">
+        <span className="actionList-item-content actionList-item--sizeMedium">
+          <span className="actionList-item-label">Medium item</span>
+        </span>
       </li>
     </ul>
   </>
@@ -131,9 +133,11 @@ SizeMedium.decorators = [
 
 export const SizeLarge = args => (
   <>
-    <ul className="actionList actionList--sizeLarge">
-      <li className="actionList-item actionList-item--sizeLarge">
-        <span className="actionList-item-content">Large item size</span>
+    <ul className="actionList">
+      <li className="actionList-item">
+        <span className="actionList-item-content actionList-item--sizeLarge">
+          <span className="actionList-item-label">Large item</span>
+        </span>
       </li>
     </ul>
   </>
@@ -165,7 +169,7 @@ SectionDivider.decorators = [
 
 export const SectionDividerFilled = args => (
   <>
-    <h4 className="actionList-sectionHeader actionList-sectionHeader--filled">Section title</h4>
+    <li className="actionList-sectionHeader actionList-sectionHeader--filled">Section title</li>
   </>
 )
 SectionDividerFilled.decorators = [
@@ -196,43 +200,15 @@ SectionDividerSimple.decorators = [
 export const LeadingVisual = args => (
   <>
     <li className="actionList-item">
-      <div className="actionList-item-content">
-        <span className="actionList-item-leadingVisual">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
-            ></path>
-          </svg>
-        </span>
+      <div className="actionList-item-content actionList-item--leadingVisual">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
+          ></path>
+        </svg>
+
         <span className="actionList-item-label">Item with leading visual</span>
-      </div>
-    </li>
-    <li className="actionList-item">
-      <div className="actionList-item-content">
-        <span className="actionList-item-leadingVisual-16">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
-            ></path>
-          </svg>
-        </span>
-        <span className="actionList-item-label">Item with leading visual 16px width</span>
-      </div>
-    </li>
-    <li className="actionList-item">
-      <div className="actionList-item-content">
-        <span className="actionList-item-leadingVisual-20">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path d="M12.75 7.75a.75.75 0 00-1.5 0v3.5h-3.5a.75.75 0 000 1.5h3.5v3.5a.75.75 0 001.5 0v-3.5h3.5a.75.75 0 000-1.5h-3.5v-3.5z"></path>
-            <path
-              fill-rule="evenodd"
-              d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"
-            ></path>
-          </svg>
-        </span>
-        <span className="actionList-item-label">Item with leading visual 20px width</span>
       </div>
     </li>
   </>
@@ -250,16 +226,15 @@ LeadingVisual.decorators = [
 export const TrailingVisual = args => (
   <>
     <li className="actionList-item">
-      <div className="actionList-item-content">
+      <div className="actionList-item-content actionList-item--trailingVisual">
         <span className="actionList-item-label">Item with trailing visual</span>
-        <span className="actionList-item-trailingVisual">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
-            ></path>
-          </svg>
-        </span>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
+          ></path>
+        </svg>
       </div>
     </li>
   </>
@@ -277,38 +252,22 @@ TrailingVisual.decorators = [
 export const LeadingAndTrailingVisual = args => (
   <>
     <li className="actionList-item">
-      <div className="actionList-item-content">
-        <span className="actionList-item-leadingVisual">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"
-            ></path>
-          </svg>
-        </span>
+      <div className="actionList-item-content actionList-item--leadingVisual actionList-item--trailingVisual">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"
+          ></path>
+        </svg>
+
         <span className="actionList-item-label">Item with leading + trailing visual</span>
-        <span className="actionList-item-trailingVisual">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
-            ></path>
-          </svg>
-        </span>
-      </div>
-    </li>
-    <li className="actionList-item">
-      <div className="actionList-item-content">
-        <span className="actionList-item-leadingVisual">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
-            <path
-              fill-rule="evenodd"
-              d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"
-            ></path>
-          </svg>
-        </span>
-        <span className="actionList-item-label">Item with leading icon + trailing text</span>
-        <span className="actionList-item-trailingText">Copy</span>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
+          ></path>
+        </svg>
       </div>
     </li>
   </>
