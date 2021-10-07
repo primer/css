71ef4a32f1c0fd0b8653563f6225537cd4b95271
langermank <langermank@github.com>
2021-10-07 16:29:27 -0700

    break out files

---
+++ docs/src/stories/ActionListDivider.stories.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import clsx from 'clsx'
+
+export default {
+  title: 'Components/ActionList/Divider',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/oMiRuexZW6gqVbMhQd6lwP/Storybook?node-id=2%3A2'
+    }
+  },
+  argTypes: {
+    variant: {
+      options: [0, 1], // iterator
+      mapping: ['', 'actionList-sectionDivider--filled'], // values
+      control: {
+        type: 'select',
+        labels: ['subtle', 'filled']
+      },
+      defaultValue: ''
+    },
+    title: {
+      defaultValue: '',
+      type: 'string',
+      name: 'title',
+      description: 'string'
+    },
+    description: {
+      defaultValue: '',
+      type: 'string',
+      name: 'description',
+      description: 'string'
+    }
+  }
+  //   decorators: [
+  //     Story => (
+  //       <div style={{margin: '3em', border: 'dashed 1px var(--color-scale-gray-3)'}}>
+  //         <ul className="actionList" role="menu">
+  //           <Story />
+  //         </ul>
+  //       </div>
+  //     )
+  //   ]
+}
+
+const Template = ({title, description, variant}) => (
+  <>
+    <li
+      className={clsx(
+        !!title && 'actionList-sectionHeader',
+        !title && 'actionList-sectionDivider',
+        variant && `${variant}`
+      )}
+      role={title ? 'heading' : 'separator'}
+    >
+      {title}
+      {description && <span className="actionList-sectionHeader--description">{description}</span>}
+    </li>
+  </>
+)
+
+export const Playground = Template.bind({})
+Playground.decorators = [
+  Story => (
+    <div style={{margin: '3rem', border: 'dashed 1px var(--color-scale-gray-3)'}}>
+      <ul className="actionList" role="menu">
+        <Story />
+      </ul>
+    </div>
+  )
+]
