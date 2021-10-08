8f47dab6016f8a500cb9dbae17e466ae01223924
Jon Rohan <yes@jonrohan.codes>
2021-10-08 09:17:08 -0700

    Removing the duplicate deprecation backgrounds

--- __tests__/css.test.js
+++ __tests__/css.test.js
@@ -2,7 +2,8 @@ import {
   getCurrentVersion,
   getPackageStats,
   getSelectorDiff,
-  getVariableDiff
+  getVariableDiff,
+  currentVersionDeprecations
 } from './utils/css'
 import semver from 'semver'
 
@@ -18,5 +19,15 @@ describe('css', () => {
   it('The support.css file contains no compiled css', () => {
     const supportStats = getPackageStats('support')
     expect(supportStats.size).toEqual(0)
+  })
+})
+
+describe('deprecations', () => {
+  it('expects deprecations and their replacement to not be equal.', () => {
+    const deprecations = currentVersionDeprecations()
+    Object.keys(deprecations["selectors"]).forEach(deprecation => {
+      const replacement = deprecations["selectors"][deprecation]
+      expect(deprecation).not.toEqual(replacement)
+    })
   })
 })
