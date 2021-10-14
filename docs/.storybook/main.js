e352f6855e368ea32f6774682252e57f19a15ec7
langermank <langermank@github.com>
2021-10-14 10:51:15 -0700

    basic storybook config

---
+++ docs/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@whitespace/storybook-addon-html',
+    'storybook-addon-designs'
+  ]
+}
