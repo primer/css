b1ab3ca20a8d570a8e9cca270bb906242f9d476b
langermank <langermank@github.com>
2021-11-02 13:25:13 -0700

    add feature stories

--- docs/src/stories/components/Button/Button.stories.jsx
+++ docs/src/stories/components/Button/Button.stories.jsx
@@ -7,31 +7,122 @@ export default {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=4371%3A7128'
-    }
+    },
+    layout: 'fullscreen'
   },
+
+  excludeStories: ['ButtonTemplate'],
   argTypes: {
     variant: {
-      options: [0, 1, 2, 3], // iterator
-      mapping: [null, 'btn-primary', 'btn-outline', 'btn-danger'], // values
+      options: [0, 1, 2, 3, 4, 5, 6, 7], // iterator
+      mapping: [
+        null,
+        'btn-primary',
+        'btn-outline',
+        'btn-danger',
+        'btn-link',
+        'btn-invisible',
+        'close-button',
+        'btn-with-count'
+      ], // values
       control: {
         type: 'select',
-        labels: ['default', 'primary', 'outline', 'danger']
+        labels: ['default', 'primary', 'outline', 'danger', 'link', 'invisible', 'close', 'with-count']
       },
-      defaultValue: ''
+      table: {
+        category: 'CSS'
+      }
+    },
+    size: {
+      options: [0, 1, 2], // iterator
+      mapping: [null, 'btn-sm', 'btn-large'], // values
+      control: {
+        type: 'select',
+        labels: ['default', 'small', 'large']
+      },
+      table: {
+        category: 'CSS'
+      }
     },
     label: {
       defaultValue: 'Button',
       type: 'string',
       name: 'label',
-      description: 'string'
+      description: 'string',
+      table: {
+        category: 'HTML'
+      }
+    },
+    disabled: {
+      defaultValue: false,
+      control: {type: 'boolean'},
+      table: {
+        category: 'Interactive'
+      }
+    },
+    fullWidth: {
+      defaultValue: false,
+      control: {type: 'boolean'},
+      table: {
+        category: 'CSS'
+      }
+    },
+    leadingVisual: {
+      name: 'leadingVisual',
+      type: 'string',
+      description: 'Paste [Octicon](https://primer.style/octicons/) in control field',
+      table: {
+        category: 'HTML'
+      }
+    },
+    trailingVisual: {
+      name: 'trailingVisual',
+      type: 'string',
+      description: 'Paste [Octicon](https://primer.style/octicons/) in control field',
+      table: {
+        category: 'HTML'
+      }
+    },
+    trailingAction: {
+      defaultValue: false,
+      control: {type: 'boolean'},
+      table: {
+        category: 'CSS'
+      }
+    },
+    selected: {
+      defaultValue: false,
+      control: {type: 'boolean'},
+      table: {
+        category: 'CSS'
+      }
     }
   }
 }
 
-const Template = ({label, variant}) => (
+export const ButtonTemplate = ({
+  label,
+  variant,
+  disabled,
+  size,
+  fullWidth,
+  leadingVisual,
+  trailingVisual,
+  trailingAction,
+  selected
+}) => (
   <>
-    <button className={clsx('btn', variant && `${variant}`)}>{label}</button>
+    <button
+      disabled={disabled}
+      className={clsx('btn', variant && `${variant}`, size && `${size}`, fullWidth && 'btn-block')}
+      aria-selected={selected}
+    >
+      {leadingVisual && <span className="" dangerouslySetInnerHTML={{__html: leadingVisual}} />}
+      <span>{label}</span>
+      {trailingVisual && <span className="" dangerouslySetInnerHTML={{__html: trailingVisual}} />}
+      {trailingAction && <span class="dropdown-caret"></span>}
+    </button>
   </>
 )
 
-export const Button = Template.bind({})
+export const Playground = ButtonTemplate.bind({})
