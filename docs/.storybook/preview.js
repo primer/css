6b4089d8b37174ca061762c56b5e4134a54cf4ef
Vinicius Depizzol <vdepizzol@github.com>
2021-12-21 12:51:59 -0800

    `PageLayout` component / Layout beta + storybook (#1737)
    * storybook for layout alpha
    
    * the beginning of layout beta
    
    * simplify spacing structure and other things
    
    this is a reminder that all the abstraction can happen in the viewComponent;
    there's no need to flood the css with constricted behaviors
    
    * add rowGap & dividers, clean spacing props
    
    * content-width support, sticky pane
    
    * cleanup props
    
    * add preset property
    
    * !default
    
    * component name & settings example
    
    * typo, cleaner header & footer template
    
    * fix preset options
    
    * mobile-friendly responsive behavior
    
    * ongoing flowVertical breakpoint behavior
    
    * finish panePosition + responsive pos & dividers
    
    * panePosition fix
    
    * add splitAsPage, finish responsive divider
    
    * Layout beta ongoing updates (#1779)
    
    - simplify component with better names
    - rename it to `PageLayout`
    - stylelint cleanup
    - finalize responsive variants
    - finalize variant-specific region dividers
    
    * add minimum 320px viewport
    
    * fix layout alpha, add layout patterns
    
    * cleanup bg colors, stylint pass
    
    * enable debug bg colors
    
    * colorful regions by default for dubugging
    
    * "Needless disable for primer/no-undefined-vars"
    
    * Stylelint auto-fixes
    
    * line breaks a EOF
    
    * introduce page layout behavior as a sb helper
    
    * children props in specialized components
    
    * cleanup
    
    * typo
    
    * discussions responsive temporary example
    
    * layout alpha descriptions
    
    * pageLayout prop descriptions
    
    * cleanup
    
    * fix conflict
    
    * copy
    
    * responsiveVariant storybook description
    
    * 0 padding on fullscreen storybook layout
    
    * has__divider for boolean props
    
    * cleanup, copy
    
    * cleanup
    
    * cleanup
    
    * cleanup chained selections/descendants
    
    * Stylelint auto-fixes
    
    * inherit values for responsive divider props
    
    * consolidate modifier names for responsive props
    
    Changelog:
    
    ### CSS classes
    - `PageLayout--variant-stackRegions` → `PageLayout--responsive-stackRegions`
    - `PageLayout--variant-separateRegions` → `PageLayout--responsive-separateRegions`
    - `PageLayout--variant-stackRegions-panePos-*` →`PageLayout--responsive-panePos-*`
    - `PageLayout--variant-separateRegions-primary-*` →`PageLayout--responsive-primary-*`
    - `PageLayout-region--hasDivider-*` → `PageLayout-region--dividerNarrow-*`
    
    ### Prop names
    - `responsivePrimaryRegion` → `primaryRegion`
    - `paneResponsivePosition` → `panePositionNarrow`
    - `paneResponsiveDivider` → `paneDividerNarrow`
    - `headerResponsiveDivider`→ `headerDividerNarrow`
    - `footerResponsiveDivider`→ `footerDividerNarrow`
    
    ### Args
    - `*DividerNarrow` props have new `inherit` value by default
    
    * Create lemon-games-swim.md
    
    * Update lemon-games-swim.md
    
    * cleanup, header+footer dividers as local modifiers
    
    * hasFooterDivider instead of footerDivider
    
    * fix primaryRegion selector
    
    Co-authored-by: Actions Auto Build <actions@github.com>
    Co-authored-by: Katie Langerman <langermank@github.com>

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -8,6 +8,13 @@ const customViewports = {
   minXS: {
     name: 'XS (min)',
     styles: {
+      width: '320px',
+      height: '100%'
+    }
+  },
+  medXS: {
+    name: 'XS (med)',
+    styles: {
       width: '375px',
       height: '100%'
     }
