c3786b850aba8d62edc766956748eb3d70fcdc69
Siddharth Kshetrapal <siddharthkp@github.com>
2022-01-19 13:08:11 +0530

    Keep Examples as the last item

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -101,7 +101,19 @@ export const parameters = {
   html: {
     root: '#story' // target id for html tab (should be direct parent of <Story /> for easy copy/paste)
   },
-  viewport: {viewports: customViewports}
+  viewport: {viewports: customViewports},
+  options: {
+    storySort: (storyA, storyB) => {
+      if (storyA[1].title.includes('Examples')) {
+        // if both are stories, sort alphabetically
+        if (storyB[1].title.includes('Examples')) return -1
+        // if only 1 is a story, push the examples story down
+        else return 1
+      }
+      // sort as usual = alphabetical
+      return -1
+    }
+  }
 }
 
 const themes = [
@@ -131,9 +143,10 @@ export const decorators = [
   (Story, context) => {
     return (
       <div class="theme-wrap">
-        { themes.map((theme) => {
-            if (context.globals.theme === theme || context.globals.theme === 'all') {
-              return <div
+        {themes.map(theme => {
+          if (context.globals.theme === theme || context.globals.theme === 'all') {
+            return (
+              <div
                 id="story"
                 className="story-wrap"
                 data-color-mode={theme.startsWith('dark') ? 'dark' : 'light'}
@@ -142,8 +155,9 @@ export const decorators = [
               >
                 <Story {...context} />
               </div>
-            }
-          })}
+            )
+          }
+        })}
       </div>
     )
   }
