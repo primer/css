12f0f61ebae455c5350e27cab1aec1ee4230d26b
Adrián Bolonio <bolonio@github.com>
2022-01-28 15:11:05 +0000

    Add new styles and open functionality to the dialog component

--- docs/src/stories/components/Dialog/Dialog.stories.jsx
+++ docs/src/stories/components/Dialog/Dialog.stories.jsx
@@ -39,38 +39,47 @@ export default {
 }
 
 const focusMethod = function getFocus() {
-  var dialog = document.getElementsByTagName('modal-dialog')[0]
+  const dialog = document.getElementsByTagName('modal-dialog')[0]
   dialog.focus()
+}
+
+const toggleDialog = () => {
+  const dialog = document.getElementById('modal-dialog-backdrop')
+  dialog.classList.toggle("modal-hide");
+  focusMethod()
 }
 
 
 export const DialogTemplate = ({title, description, focusElement}) => (
-  <div class="modal-dialog-backdrop">
-    <modal-dialog class="modal-dialog" width="xlarge" height="auto" role="dialog" aria-labelledby="react-aria8832355892-4" aria-describedby="react-aria8832355892-5" data-focus-trap="active" open>
-      <header class="dialog-header">
-        <div class="header-container">
-          <div class="header-container-title">
-            <h1 id={`dialog-title`} class="title">{title}</h1>
-            {description &&
-              <h2 id={`dialog-description`} class="subtitle">{description}</h2>
-            }
+  <>
+    <button class="btn modal-button" onClick={toggleDialog}><span>Open dialog</span></button>
+    <div id="modal-dialog-backdrop" class="modal-dialog-backdrop modal-hide">
+      <modal-dialog class="modal-dialog " width="xlarge" height="auto" role="dialog" aria-labelledby="react-aria8832355892-4" aria-describedby="react-aria8832355892-5" data-focus-trap="active" open>
+        <header class="dialog-header">
+          <div class="header-container">
+            <div class="header-container-title">
+              <h1 id={`dialog-title`} class="title">{title}</h1>
+              {description &&
+                <h2 id={`dialog-description`} class="subtitle">{description}</h2>
+              }
+            </div>
+            <button class="close-button" aria-label="Close" onClick={toggleDialog}>
+              <svg aria-hidden="true" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style={{"display": "inline-block", "user-select": "none", "vertical-align": "text-bottom", "overflow": "visible"}}>
+                <path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
+              </svg>
+            </button>
           </div>
-          <button class="close-button" aria-label="Close">
-            <svg aria-hidden="true" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style={{"display": "inline-block", "user-select": "none", "vertical-align": "text-bottom", "overflow": "visible"}}>
-              <path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
-            </svg>
-          </button>
+        </header>
+        <div class="dialog-body">
+          This is the body of the dialog
         </div>
-      </header>
-      <div class="dialog-body">
-        This is the body of the dialog
-      </div>
-      <footer class="dialog-footer">
-        <button class="btn"><span>Continue</span></button>
-      </footer>
-    </modal-dialog>
-    {focusElement && focusMethod()}
-  </div>
+        <footer class="dialog-footer">
+          <button class="btn" onClick={toggleDialog}><span>Continue</span></button>
+        </footer>
+      </modal-dialog>
+      {focusElement && focusMethod()}
+    </div>
+  </>
 )
 
 export const Playground = DialogTemplate.bind({})
