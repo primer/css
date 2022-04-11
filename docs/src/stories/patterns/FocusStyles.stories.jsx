aa7e158389a811565ff2566806876ddf589b8e65
langermank <langermank@github.com>
2022-04-11 11:21:39 -0700

    moving specific styles from dotcom

--- docs/src/stories/patterns/FocusStyles.stories.jsx
+++ docs/src/stories/patterns/FocusStyles.stories.jsx
@@ -25,6 +25,12 @@ export const FocusStyles = ({}) => (
       <ButtonTemplate variant="btn-danger" label="Danger" />
       <ButtonTemplate variant="btn-link" label="Link" />
       <ButtonTemplate variant="btn-invisible" label="Invisible" />
+      <ButtonTemplate
+        variant="btn-octicon"
+        leadingVisual={`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+            <path d="M8 16c.9 0 1.7-.6 1.9-1.5.1-.3-.1-.5-.4-.5h-3c-.3 0-.5.2-.4.5.2.9 1 1.5 1.9 1.5zM3 5c0-2.8 2.2-5 5-5s5 2.2 5 5v3l1.7 2.6c.2.2.3.5.3.8 0 .8-.7 1.5-1.5 1.5h-11c-.8.1-1.5-.6-1.5-1.4 0-.3.1-.6.3-.8L3 8.1V5z"></path>
+          </svg>`}
+      />
       <ButtonTemplate variant="btn-primary" label="Primary" disabled />
       <ButtonTemplate variant="btn-secondary" label="Secondary" disabled />
       <ButtonTemplate variant="btn-outline" label="Outline" disabled />
@@ -38,7 +44,8 @@ export const FocusStyles = ({}) => (
       <InputTemplate label="input" type="text" />
       <SelectTemplate label="select" />
       <TextareaTemplate label="textarea" />
-      <LinkTemplate label="Link" href="/" />
+      <LinkTemplate label="Primer link" href="/" />
+      <a href="/">Link with no CSS class</a>
       <MarketingButtonTemplate label="Marketing Button" />
       <MarketingLinkTemplate label="Marketing Link" href="/" />
     </div>
