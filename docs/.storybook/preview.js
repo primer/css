fe4e6b530e8b158fa53738f859696794c789d6da
langermank <langermank@github.com>
2022-05-18 14:40:58 -0700

    add storybook support

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -2,6 +2,17 @@ import '../../src/docs.scss'
 import '../../src/index.scss'
 import '../../src/base/index.scss'
 import '../src/stories/helpers/storybook-styles.scss'
+// temporary import until primitives moves to core bundle
+// importing the index from /css didn't play nice with Storybook
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/base/size/size.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/base/typography/typography.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/border.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/breakpoints.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/size-coarse.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/size-fine.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/size.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/size/viewport.css'
+import '../../node_modules/@primer/primitives/tokens-v2-private/css/tokens/functional/typography/typography.css'
 import renderToHTML from '../src/stories/helpers/code-snippet-html-helper'
 
 const customViewports = {
