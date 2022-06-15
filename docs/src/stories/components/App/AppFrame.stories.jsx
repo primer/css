1d3ef393fc6ea15b93f66f53f93287927d3c70bc
Vinicius Depizzol <vdepizzol@gmail.com>
2022-06-15 16:15:57 -0700

    Markup cleanup

--- docs/src/stories/components/App/AppFrame.stories.jsx
+++ docs/src/stories/components/App/AppFrame.stories.jsx
@@ -17,11 +17,8 @@ export default {
   excludeStories: ['AppFrameTemplate'],
   argTypes: {
 
-    // Banner tray
-
-    bannerType: {
-      control: 'radio',
-      options: ['none', 'info', 'warning', 'error', 'success'],
+    a11yNavItems: {
+      type: 'array',
     },
 
     // Children
@@ -48,118 +45,60 @@ export default {
 }
 
 export const AppFrameTemplate = ({
-  //hasServerStats,
-  //bannerType,
-  //alertMessage,
+  a11yNavItems,
   headerChildren,
   bodyChildren,
   footerChildren,
-}) => (
-  <>
-    <div className={clsx('AppFrame')}>
+}) => {
 
-      <div className={clsx('AppFrame-a11yNav')}>
-        <ul>
-          <li><a href="#start-of-content" className={clsx('AppFrame-a11yLink')}>Skip to content</a></li>
-          <li><a href="/" className={clsx('AppFrame-a11yLink')}>GitHub homepage</a></li>
-        </ul>
+  // Default values
+  a11yNavItems = a11yNavItems ?? [
+    {url: '#start-of-content', label: 'Skip to content'},
+    {url: '/', label: 'GitHub homepage'},
+  ];
 
-      </div>
+  return (
+    <>
+      <div className={clsx('AppFrame')}>
 
-      <div className={clsx('AppFrame-main')}>
+        <div className={clsx('AppFrame-a11yNav')}>
+          <ul>
+            {a11yNavItems.map(link => (
+              <li>
+                <a className={clsx('AppFrame-a11yLink')} href={link.url}>{link.label}</a>
+              </li>
+            ))}
+          </ul>
 
-        <div className={clsx('AppFrame-header-wrapper')}>
+        </div>
 
-          <div className={clsx('AppFrame-header')}>
-            {headerChildren}
-          </div>
+        <div className={clsx('AppFrame-main')}>
 
-          <div id="start-of-content"></div>
+          <div className={clsx('AppFrame-header-wrapper')}>
 
-          {/* {bannerType !== undefined && bannerType !== 'none' && (
-            <div className={clsx('AppFrame-tray')}>
-                <div className="container-md">
-                  <FlashTemplate variant={bannerType} message={alertMessage} hasVisual={true} hasCloseButton={true} isFullWhenNarrow={true} />
-                </div>
+            <div className={clsx('AppFrame-header')}>
+              {headerChildren}
             </div>
-          )} */}
+
+            <div id="start-of-content"></div>
+
+            {/* Todo: figure out region for banners / custom elements that live before AppFrame-body */}
+          </div>
+          <div className={clsx('AppFrame-body')}>
+            {bodyChildren}
+          </div>
         </div>
-        <div className={clsx('AppFrame-body')}>
-          {bodyChildren}
+        <div className={clsx('AppFrame-footer')}>
+          {footerChildren}
         </div>
       </div>
-      <div className={clsx('AppFrame-footer')}>
-        {footerChildren}
-      </div>
-    </div>
-  </>
-)
-
-const headerClassic =
-  <>
-    <div className={clsx('Header')}>
-      <div className="Header-item">
-        <a href="#" className="Header-link f4 d-flex flex-items-center">
-          <svg height="32" className="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" fill="#fff"></path></svg>
-        </a>
-      </div>
-      <div className="Header-item Header-item--full">
-        {/* Menu */}
-      </div>
-      <div className="Header-item mr-0">
-        <img className="avatar d-block" height="32" alt="@octocat" src="https://github.com/octocat.png" width="32" class="circle" />
-      </div>
-    </div>
-  </>;
-
-const headerThemeable =
-  <>
-    <div className={clsx('Header', 'Header--matchTheme')}>
-      <div className="Header-item">
-        <a href="#" className="Header-logo">
-          <svg height="32" className="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
-        </a>
-      </div>
-      <div className="Header-item Header-item--full">
-        {/* Menu */}
-      </div>
-      <div className="Header-item mr-0">
-        <a href="#" className="Header-user">
-          <img className="avatar" height="32" alt="@octocat" src="https://github.com/octocat.png" width="32" class="circle" />
-        </a>
-      </div>
-    </div>
-  </>;
-
-const headerPrimer =
-<>
-  <div className={clsx('Header', 'Header--matchTheme')}>
-    <div className="Header-item">
-      <a href="#" className="Header-logo">
-        <svg height="32" className="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true" style={{'fill': 'var(--color-accent-fg)'}}><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
-      </a>
-      <h1 className="Header-title">
-        <a href="#">Primer</a>
-      </h1>
-    </div>
-    <div className="Header-item Header-item--full">
-      {/* Menu */}
-    </div>
-    <div className="Header-item mr-0">
-      <a href="#" className="Header-user">
-        <img className="avatar" height="32" alt="@octocat" src="https://github.com/octocat.png" width="32" class="circle" />
-      </a>
-    </div>
-  </div>
-</>;
-
+    </>
+  );
+};
 
 export const Playground = AppFrameTemplate.bind({})
 
 Playground.args = {
-  hasServerStats: false,
-  headerTemplate: 'classic',
-  bannerType: 'none',
   headerChildren:
     <>
       <AppHeader localBarChildren={
--- docs/src/stories/components/App/AppHeader.stories.jsx
+++ docs/src/stories/components/App/AppHeader.stories.jsx
@@ -15,6 +15,16 @@ export default {
       control: 'boolean',
     },
 
+    userHasIndicator: {
+      control: 'boolean',
+    },
+
+    context: {
+      control: {
+        type: 'object',
+      }
+    },
+
     localBarChildren: {
       control: 'text',
     }
@@ -24,161 +34,174 @@ export default {
 
 export const AppHeaderTemplate = ({
   notificationsHasIndicator,
+  userHasIndicator,
+  context,
   localBarChildren,
-}) => (
-  <>
-    <div className={clsx('AppHeader')}>
+}) => {
 
-      {/*
-        * AppHeader-globalBar
-        * -----------------
-        * The GitHub global bar contains the logo and user avatar, with
-        * contextual navigation + actions
-        */}
-      <div className={clsx('AppHeader-globalBar')}>
+  // Default values
+  notificationsHasIndicator = notificationsHasIndicator ?? false;
+  userHasIndicator = userHasIndicator ?? false;
+  context = context ?? [
+    {url: '/primer', label: 'primer'},
+    {url: '/primer/components', label: 'components'},
+  ];
 
-        <div className={clsx('AppHeader-globalBar-start')}>
-          {/*
-            * AppHeader-logo
-            * -----------------
-            * The GitHub interactive logo region
-            */}
-          <div className="AppHeader-logo">
+  return (
+    <>
+      <div className={clsx('AppHeader')}>
+
+        {/*
+          * AppHeader-globalBar
+          * -----------------
+          * The GitHub global bar contains the logo and user avatar, with
+          * contextual navigation + actions
+          */}
+        <div className={clsx('AppHeader-globalBar')}>
+
+          <div className={clsx('AppHeader-globalBar-start')}>
             {/*
-              * The button element can be focusable. Click event opens side sheet
-              * with main global menu. A direct link to the GitHub homepage is
-              * available before the global header, in the AppFrame component.
-              * */}
-            <button aria-expanded="false" aria-haspopup="dialog" aria-label="Open main menu">
-              {/*
-                * Replace with ViewComponent:
-                * <%= render(Primer::OcticonComponent.new(:"mark-github")) %>
-                */}
-              <MarkGithubIcon />
-            </button>
-
-            {/*
-              * The anchor element is not focusable, yet handles mouse pointer
-              * events such as ctrl+click, middle click, right-click, for users to
-              * handle the homepage URL when interacting with the website logo as
-              * they're used to.
-              * 
-              * See https://codepen.io/team/GitHub/pen/OJQgyqo for interactive
-              * example including the expected `mouseup` event listener.
+              * AppHeader-logo
+              * -----------------
+              * The GitHub interactive logo region
               */}
-            <a href="/" tabindex="-1" aria-hidden>GitHub homepage</a>
-          </div>
-
-          {/*
-            * AppHeader-context
-            * -----------------
-            * Required region right after the GitHub logo. May be a breadcrumb or
-            * page title, placed left-aligned.
-            */}
-          <div className="AppHeader-context">
-            <div className="AppHeader-context-compact">
-              <a href="#">
-                <span className="AppHeader-context-compact-lead">
-                  <span className="AppHeader-context-compact-lead--parentItem">github</span>/
-                </span>
-                <strong className="AppHeader-context-compact-lead--mainItem">docs</strong>
-              </a>
-            </div>
-            <div className="AppHeader-context-full">
-              <nav aria-label="">
-                <ul>
-                  <li><a className="AppHeader-context-item" href="#">github<span className="sr-only">/</span></a></li>
-                  <li><a className="AppHeader-context-item" href="#">docs</a></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>
-
-        <div className={clsx('AppHeader-globalBar-end')}>
-
-          {/*
-            * AppHeader-search
-            * -----------------
-            * This region shows a search input on regular viewports, and search
-            * button on narrow viewports.
-            */}
-          <div className="AppHeader-search">
-            {/* Narrow viewport-specific search button */}
-            <button className={clsx('AppHeader-button', 'AppHeader-search-whenNarrow')} aria-label="Search or jump to...">
+            <div className="AppHeader-logo">
               {/*
-                * Replace with ViewComponent:
-                * <%= render(Primer::OcticonComponent.new(:"search")) %>
-                */}
-              <SearchIcon />
-            </button>
-
-            {/* Regular viewport search input */}
-            <form className="AppHeader-search-whenRegular">
-              
-              <div className="AppHeader-search-wrap AppHeader-search-wrap--hasTrailing">
-                {/*
-                  * AppHeader-search-wrap needs to have two direct children:
-                  * - AppHeader-search-label
-                  * - AppHeader-search-control
-                  */}
-                <label className="AppHeader-search-label" for="AppHeader-searchInput">Type <kbd>/</kbd> to search</label>
-                <div className="AppHeader-search-control">
-                  <span className="AppHeader-search-visual--leading"><SearchIcon /></span>
-                  <input name="q" className="form-control input-contrast" id="AppHeader-searchInput" type="search" placeholder="Search or jump to..." />
-                </div>
-                <button className="AppHeader-search-action--trailing">
+                * The button element can be focusable. Click event opens side sheet
+                * with main global menu. A direct link to the GitHub homepage is
+                * available before the global header, in the AppFrame component.
+                * */}
+              <button aria-expanded="false" aria-haspopup="dialog" aria-label="Open main menu">
                 {/*
                   * Replace with ViewComponent:
-                  * <%= render(Primer::OcticonComponent.new(:"xxxxx")) %>
+                  * <%= render(Primer::OcticonComponent.new(:"mark-github")) %>
                   */}
-                  <TerminalIcon />
-                </button>
+                <MarkGithubIcon />
+              </button>
+
+              {/*
+                * The anchor element is not focusable, yet handles mouse pointer
+                * events such as ctrl+click, middle click, right-click, for users to
+                * handle the homepage URL when interacting with the website logo as
+                * they're used to.
+                * 
+                * See https://codepen.io/team/GitHub/pen/OJQgyqo for interactive
+                * example including the expected `mouseup` event listener.
+                */}
+              <a href="/" tabindex="-1" aria-hidden>GitHub homepage</a>
+            </div>
+
+            {/*
+              * AppHeader-context
+              * -----------------
+              * Required region right after the GitHub logo. May be a breadcrumb or
+              * page title, placed left-aligned.
+              */}
+            <div className="AppHeader-context">
+              <div className="AppHeader-context-compact">
+                <a href="#">
+                  <span className="AppHeader-context-compact-lead">
+                    <span className="AppHeader-context-compact-lead--parentItem">loremipsumdolorsitametavecconsequer</span>/
+                  </span>
+                  <strong className="AppHeader-context-compact-lead--mainItem">loremipsumdolorsitametavecconsequer</strong>
+                </a>
               </div>
-            </form>
-          </div>
-          
-          <div className="AppHeader-actions">
-            <a className={clsx('AppHeader-button', notificationsHasIndicator && 'AppHeader-button--hasIndicator')} href="#">
-              {/*
-                * Replace with ViewComponent:
-                * <%= render(Primer::OcticonComponent.new(:"inbox")) %>
-                */}
-              <InboxIcon />
-            </a>
-            
-            <a className="AppHeader-button" href="#">
-              {/*
-                * Replace with ViewComponent:
-                * <%= render(Primer::OcticonComponent.new(:"plus")) %>
-                */}
-              <PlusIcon />
-            </a>
+              <div className="AppHeader-context-full">
+                <nav aria-label="">
+                  <ul>
+                    <li><a className="AppHeader-context-item" href="#">loremipsumdolorsitametavecconsequer<span className="sr-only">/</span></a></li>
+                    <li><a className="AppHeader-context-item" href="#">loremipsumdolorsitametavecconsequer</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
           </div>
 
-          {/*
-            * AppHeader-user
-            * --------------
-            * Same as the AppHeader-logo structure, with button and anchor
-            * elements working together to enable fully accessible interaction
-            * with keyboard + mouse
-            */}
-          <div class="AppHeader-user">
-            <button aria-expanded="false" aria-haspopup="dialog" aria-label="Open user menu">
-              <img alt="@octocat" src="https://github.com/octocat.png" />
-            </button>
+          <div className={clsx('AppHeader-globalBar-end')}>
+
+            {/*
+              * AppHeader-search
+              * -----------------
+              * This region shows a search input on regular viewports, and search
+              * button on narrow viewports.
+              */}
+            <div className="AppHeader-search">
+              {/* Narrow viewport-specific search button */}
+              <button className={clsx('AppHeader-button', 'AppHeader-search-whenNarrow')} aria-label="Search or jump to...">
+                {/*
+                  * Replace with ViewComponent:
+                  * <%= render(Primer::OcticonComponent.new(:"search")) %>
+                  */}
+                <SearchIcon />
+              </button>
+
+              {/* Regular viewport search input */}
+              <form className="AppHeader-search-whenRegular">
+                
+                <div className="AppHeader-search-wrap AppHeader-search-wrap--hasTrailing">
+                  {/*
+                    * AppHeader-search-wrap needs to have two direct children:
+                    * - AppHeader-search-label
+                    * - AppHeader-search-control
+                    */}
+                  <label className="AppHeader-search-label" for="AppHeader-searchInput">Type <kbd>/</kbd> to search</label>
+                  <div className="AppHeader-search-control">
+                    <span className="AppHeader-search-visual--leading"><SearchIcon /></span>
+                    <input name="q" className="form-control input-contrast" id="AppHeader-searchInput" type="search" placeholder="Search or jump to..." />
+                  </div>
+                  <button className="AppHeader-search-action--trailing">
+                  {/*
+                    * Replace with ViewComponent:
+                    * <%= render(Primer::OcticonComponent.new(:"xxxxx")) %>
+                    */}
+                    <TerminalIcon />
+                  </button>
+                </div>
+              </form>
+            </div>
             
-            <a href="/octocat" tabindex="-1" aria-hidden>octocat profile</a>
+            <div className="AppHeader-actions">
+              <a className={clsx('AppHeader-button', notificationsHasIndicator && 'AppHeader-button--hasIndicator')} href="#">
+                {/*
+                  * Replace with ViewComponent:
+                  * <%= render(Primer::OcticonComponent.new(:"inbox")) %>
+                  */}
+                <InboxIcon />
+              </a>
+              
+              <a className="AppHeader-button" href="#">
+                {/*
+                  * Replace with ViewComponent:
+                  * <%= render(Primer::OcticonComponent.new(:"plus")) %>
+                  */}
+                <PlusIcon />
+              </a>
+            </div>
+
+            {/*
+              * AppHeader-user
+              * --------------
+              * Same as the AppHeader-logo structure, with button and anchor
+              * elements working together to enable fully accessible interaction
+              * with keyboard + mouse
+              */}
+            <div class="AppHeader-user">
+              <button aria-expanded="false" aria-haspopup="dialog" aria-label="Open user menu">
+                <img alt="@octocat" src="https://github.com/octocat.png" />
+              </button>
+              
+              <a href="/octocat" tabindex="-1" aria-hidden>octocat profile</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={clsx('AppHeader-localBar')}>
-        {localBarChildren}
+        <div className={clsx('AppHeader-localBar')}>
+          {localBarChildren}
+        </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  );
+};
 
 export const Playground = AppHeaderTemplate.bind({})
 
