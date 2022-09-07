dbe0aa09f26375082c3eac43f994e09c71cc4552
langermank <langermank@github.com>
2022-09-07 14:55:26 -0700

    add stories

--- docs/src/stories/utilities/Typography.stories.jsx
+++ docs/src/stories/utilities/Typography.stories.jsx
@@ -1,74 +1,51 @@
 import React from 'react'
-import clsx from 'clsx'
-// import { StoryTemplateName } from './OtherStoryFile.stories' // import stories for component compositions
 
 export default {
   title: 'Utilities/Typography',
-  excludeStories: ['TypographyTemplate'],
-  layout: 'padded',
-  argTypes: {
-    booleanExample: {
-      control: {type: 'boolean'},
-      description: 'true/false toggle to controls',
-      table: {
-        category: 'Pick one: CSS, HTML, Interactive'
-      }
-    },
-    radioExample: {
-      options: ['string1', 'string2', 'string3', 'string4'],
-      control: {
-        type: 'inline-radio'
-      },
-      description: 'radio buttons mapping to strings (example: use for variant class names)',
-      table: {
-        category: 'Pick one: CSS, HTML, Interactive'
-      }
-    },
-    stringExample: {
-      name: 'stringExample',
-      type: 'string',
-      description: 'text box control',
-      table: {
-        category: 'Pick one: CSS, HTML, Interactive'
-      }
-    },
-    children: {
-      description: 'creates a slot for children',
-      table: {
-        category: 'HTML'
-      }
-    }
-  }
+  layout: 'padded'
 }
 
 // build every component case here in the template (private api)
-export const TypographyTemplate = ({booleanExample, radioExample, stringExample, children}) => (
-  <div
-    // use clsx for multiple classnames
-    className={clsx('defaultClass', radioExample && `radioExample`, booleanExample && 'defaultClass--modifier')}
-    // use undefined for values that shouldn't be set if false
-    // aria-hidden={booleanExample ? 'true' : undefined}
-  >
-    {/* use {children} for wrapper component templates */}
-    <>
-      <h1 class="h1">Pizza 1</h1>
-      <h2 class="h2">Pizza 2</h2>
-      <h3 class="h3">Pizza 3</h3>
-      <h4 class="h4">Pizza 4</h4>
-      <h5 class="h5">Pizza 5</h5>
-      <h6 class="h6">Pizza 6</h6>
-    </>
+export const SemanticScale = () => (
+  <div>
+    <h1>Heading one</h1>
+    <h2>Heading two</h2>
+    <h3>Heading three</h3>
+    <h4>Heading four</h4>
+    <h5>Heading five</h5>
+    <h6>Heading six</h6>
   </div>
 )
 
-// create a "playground" demo page that may set some defaults and allow story to access component controls
-export const Playground = TypographyTemplate.bind({})
-Playground.args = {
-  stringExample: 'Default text',
-  booleanExample: false
-  //   children: (
-  //     <>
-  //       <StoryTemplateName {...StoryTemplateName.args} />
-  //     </>
-  //   )
-}
+export const ResponsiveUtilities = () => (
+  <div>
+    <h1 class="h1">Heading one</h1>
+    <h2 class="h2">Heading two</h2>
+    <h3 class="h3">Heading three</h3>
+    <h4 class="h4">Heading four</h4>
+    <h5 class="h5">Heading five</h5>
+    <h6 class="h6">Heading six</h6>
+  </div>
+)
+
+export const ResponsiveUtilitiesNoWeight = () => (
+  <div>
+    <h1 class="f1">Heading one</h1>
+    <h2 class="f2">Heading two</h2>
+    <h3 class="f3">Heading three</h3>
+    <h4 class="f4">Heading four</h4>
+    <h5 class="f5">Heading five</h5>
+    <h6 class="f6">Heading six</h6>
+  </div>
+)
+
+export const ResponsiveUtilitiesLightWeight = () => (
+  <div>
+    <h1 class="f1-light">Heading one</h1>
+    <h2 class="f2-light">Heading two</h2>
+    <h3 class="f3-light">Heading three</h3>
+    <h4 class="f4-light">Heading four</h4>
+    <h5 class="f5-light">Heading five</h5>
+    <h6 class="f6-light">Heading six</h6>
+  </div>
+)
