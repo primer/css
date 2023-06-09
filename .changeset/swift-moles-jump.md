352ed7b75585c686c996a5e7c2c29e20e41d0672
Katie Langerman <18661030+langermank@users.noreply.github.com>
2023-06-09 11:13:04 -0700

    Add new PostCSS fallback config (#2457)
    * add new fallback config
    
    * Create swift-moles-jump.md

---
+++ .changeset/swift-moles-jump.md
@@ -0,0 +1,5 @@
+---
+"@primer/css": patch
+---
+
+Add new PostCSS fallback config
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,8 @@ const sass = require('@csstools/postcss-sass')
 const scss = require('postcss-scss')
 const scssImport = require('postcss-import')
 const { join } = require('path')
-const importedJSONFromPrimitives = require('./node_modules/@primer/primitives/tokens-next-private/fallbacks/color-fallbacks.json')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
   map: {
@@ -20,9 +21,29 @@ module.exports = {
     }),
     autoprefixer,
     require('postcss-custom-properties-fallback')({
-      importFrom: {
-        customProperties: importedJSONFromPrimitives
-      },
+      importFrom: [
+        () => {
+          const primitiveFallbacks = [
+            'color-fallbacks.json',
+            'base/size/size.json',
+            'base/typography/typography.json',
+            'functional/size/border.json',
+            'functional/size/breakpoints.json',
+            'functional/size/size-coarse.json',
+            'functional/size/size-fine.json',
+            'functional/size/size.json',
+            'functional/size/viewport.json',
+            'functional/typography/typography.json',
+          ]
+          let customProperties = {}
+          for (const filePath of primitiveFallbacks) {
+            const fileData = fs.readFileSync(path.join(__dirname, './node_modules/@primer/primitives/tokens-next-private/fallbacks/', filePath), 'utf8')
+            customProperties = {...customProperties, ...JSON.parse(fileData)}
+          }
+
+          return { customProperties: customProperties };
+        }
+      ]
     }),
   ]
 }
