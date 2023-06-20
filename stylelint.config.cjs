99b6f35f920055aaef7249c4bc56cc30a35236e6
langermank <18661030+langermank@users.noreply.github.com>
2023-06-20 11:10:59 -0700

    bump

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,6 +2,12 @@ module.exports = {
   extends: ['@primer/stylelint-config'],
   ignoreFiles: ['**/*.js', '**/*.cjs'],
   rules: {
-    'primer/no-override': false
-  }
+    'primer/no-override': false,
+    'primer/no-deprecated-colors': [
+      true,
+      {
+        inlineFallback: true,
+      },
+    ],
+  },
 }
