c078248fb2499908b9ff2ee7a8dfad39e86f7556
langermank <18661030+langermank@users.noreply.github.com>
2024-04-08 12:08:52 -0700

    cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,8 +3,6 @@ const sass = require('@csstools/postcss-sass')
 const scss = require('postcss-scss')
 const scssImport = require('postcss-import')
 const { join } = require('path')
-const path = require('path')
-const fs = require('fs')
 
 module.exports = {
   map: {
@@ -19,31 +17,6 @@ module.exports = {
       includePaths: [join(__dirname, 'node_modules')],
       outputStyle: process.env.CSS_MINIFY === '0' ? 'expanded' : 'compressed'
     }),
-    autoprefixer,
-    require('postcss-custom-properties-fallback')({
-      importFrom: [
-        () => {
-          const primitiveFallbacks = [
-            'color-fallbacks.json',
-            'base/size/size.json',
-            'base/typography/typography.json',
-            'functional/size/border.json',
-            'functional/size/breakpoints.json',
-            'functional/size/size-coarse.json',
-            'functional/size/size-fine.json',
-            'functional/size/size.json',
-            'functional/size/viewport.json',
-            'functional/typography/typography.json',
-          ]
-          let customProperties = {}
-          for (const filePath of primitiveFallbacks) {
-            const fileData = fs.readFileSync(path.join(__dirname, './node_modules/@primer/primitives/tokens-next-private/fallbacks/', filePath), 'utf8')
-            customProperties = {...customProperties, ...JSON.parse(fileData)}
-          }
-
-          return { customProperties: customProperties };
-        }
-      ]
-    }),
+    autoprefixer
   ]
 }
