c3942f43e7da271f0a42ff9a84b2753d1359e5b1
Jon Rohan <yes@jonrohan.codes>
2024-06-03 15:43:47 +0000

    upgrade

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,13 +1,5 @@
 module.exports = {
   extends: ['@primer/stylelint-config'],
   ignoreFiles: ['**/*.js', '**/*.cjs'],
-  rules: {
-    'primer/no-override': false,
-    'primer/no-deprecated-colors': [
-      true,
-      {
-        inlineFallback: true,
-      },
-    ],
-  },
+  rules: {}
 }
